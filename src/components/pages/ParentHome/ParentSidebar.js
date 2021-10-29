b93123245804c71a43c1a437722516322f4eebc3
cn8817 <christinenguyen1433@gmail.com>
2021-10-29 10:23:48 -0700

    Added sidebar with links to: Overview, Progress, Achievements, Student's Tasks, Resources, Messaging, and Booking into the Parent Dashboard

---
+++ src/components/pages/ParentHome/ParentSidebar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Menu } from 'antd';
+import 'antd/dist/antd.css';
+
+//const { SubMenu } = Menu; if submenu is needed in the sidebar
+
+function ParentSidebar() {
+  const handleClick = e => {
+    console.log('click ', e);
+  };
+
+  return (
+    <Menu
+      onClick={handleClick}
+      style={{ width: 256 }}
+      defaultSelectedKeys={['1']}
+      defaultOpenKeys={['sub1']}
+      mode="inline"
+    >
+      <Menu.Item key="1">Overview</Menu.Item>
+      <Menu.Item key="2">Progress</Menu.Item>
+      <Menu.Item key="3">Achievements</Menu.Item>
+      <Menu.Item key="4">Student's Tasks</Menu.Item>
+      <Menu.Item key="5">Resources</Menu.Item>
+      <Menu.Item key="6">Messaging</Menu.Item>
+      <Menu.Item key="7">Booking</Menu.Item>
+    </Menu>
+  );
+}
+export default ParentSidebar;

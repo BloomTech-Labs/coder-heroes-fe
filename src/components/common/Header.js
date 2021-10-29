12c54059de0d20722e88eb4a7c8452905fe54098
Michael <maikurusama@gmail.com>
2021-10-29 16:12:09 -0400

    Adding links for the header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,9 +5,11 @@ export default function MainHeader({ props }) {
   return (
     <div>
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
+        <Menu.Item key="1">Programs</Menu.Item>
+        <Menu.Item key="2">Instructors</Menu.Item>
+        <Menu.Item key="3">Booking</Menu.Item>
+        <Menu.Item key="3">Scholarships</Menu.Item>
+        <Menu.Item key="3">Contact Us (button)</Menu.Item>
       </Menu>
     </div>
   );

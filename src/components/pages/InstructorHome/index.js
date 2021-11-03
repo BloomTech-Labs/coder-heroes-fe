e5cd9588289c195ec5ba71a3edb3e2907933ba8b
Rodgers31 <otienor986@gmail.com>
2021-11-02 22:29:35 -0400

    Used dummy data to populate calender and passed props to the needed components

--- src/components/pages/InstructorHome/index.js
+++ src/components/pages/InstructorHome/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import InstructorSidebar from './InstructorSidebar';
 import InstructorCalender from './InstructorCalender';
 import InstructorProfile from './InstructorProfile';
+import { dummyData } from '../../../dummyData';
 
 const InstructorHome = () => {
   return (
     <div>
       <InstructorSidebar />
-      <InstructorCalender />
-      <InstructorProfile />
+      <InstructorCalender dummyData={dummyData.courses} />
+      <InstructorProfile dummyData={dummyData.instructor} />
     </div>
   );
 };

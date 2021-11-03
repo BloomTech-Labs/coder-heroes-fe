38dede9ffb88aa18b8455aacdf970f849767cb5c
Rodgers31 <39386788+Rodgers31@users.noreply.github.com>
2021-11-02 22:44:53 -0400

    Update index.js

--- src/components/pages/InstructorHome/index.js
+++ src/components/pages/InstructorHome/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import InstructorSidebar from './InstructorSidebar';
 import InstructorCalender from './InstructorCalender';
 import InstructorProfile from './InstructorProfile';
-import { dummyData } from '../../../dummyData';
+
 
 const InstructorHome = () => {
   return (

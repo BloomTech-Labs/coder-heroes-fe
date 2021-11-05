761f4b48743d736326c4eef803de0d7391043d34
cn8817 <christinenguyen1433@gmail.com>
2021-11-05 15:01:04 -0700

    imported connect into the ChildrenList, mapped state to props, and cleaned up the ParentHome index

--- src/components/pages/ParentHome/index.js
+++ src/components/pages/ParentHome/index.js
@@ -5,11 +5,10 @@ import ParentSidebar from './ParentSidebar';
 import { dummyData } from '../../../dummyData';
 
 function ParentHome() {
-  const [parentData, setParentData] = useState(dummyData);
   const [scheduleData, setScheduleData] = useState(dummyData);
   return (
     <div className="App">
-      <ChildrenList children={parentData}></ChildrenList>
+      <ChildrenList></ChildrenList>
       <ParentCalendar schedule={scheduleData}></ParentCalendar>
       <ParentSidebar></ParentSidebar>
     </div>

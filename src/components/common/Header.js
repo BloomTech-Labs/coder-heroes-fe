6a616e30d5e8f11c056d85779793d44f6520d363
Fernando Martinez <fernando817mm@gmail.com>
2021-11-05 14:23:02 -0500

    Added logout button to navbar as well as it's functionality. Removed logout button from RenderHomePage.js

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { Menu } from 'antd';
+import { useOktaAuth } from '@okta/okta-react';
 
-export default function MainHeader({ props }) {
+export default function MainHeader(props) {
+  const { authService, oktaAuth } = useOktaAuth();
+  console.log(localStorage.getItem('okta-token-storage').length);
+
   return (
     <div>
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
         <Menu.Item key="1">Programs</Menu.Item>
         <Menu.Item key="2">Instructors</Menu.Item>
         <Menu.Item key="3">Booking</Menu.Item>
-        <Menu.Item key="3">Scholarships</Menu.Item>
-        <Menu.Item key="3">Contact Us (button)</Menu.Item>
+        <Menu.Item key="4">Scholarships</Menu.Item>
+        <Menu.Item key="5">Contact Us (button)</Menu.Item>
+        {localStorage.getItem('okta-token-storage').length > 2 ? (
+          <Menu.Item
+            key="6"
+            onClick={() => {
+              // authService.logout();
+              // oktaAuth.tokenManager.clear();
+            }}
+          >
+            Logout
+          </Menu.Item>
+        ) : (
+          <></>
+        )}
       </Menu>
     </div>
   );

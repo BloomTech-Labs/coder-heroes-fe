74e84f41a21fd4aea65b0a1d9ae503ad42cfcdd3
Alejandro Vasquez <alejandrovasquez@Alejandros-iMac.local>
2021-11-07 19:49:22 -0700

    MapStateToProps in the parent calendar so that props are coming from the parent reducer rather than the dummy data file

--- src/components/pages/ParentHome/ParentCalendar.js
+++ src/components/pages/ParentHome/ParentCalendar.js
@@ -1,70 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import 'antd/dist/antd.css';
 
 import { Calendar, Badge } from 'antd';
 
+import { connect } from 'react-redux';
+
 function ParentCalendar(props) {
   const { schedule } = props;
+  const [course, setCourse] = useState('');
+  const [time, setTime] = useState('');
 
   function getListData(value) {
+    const values = schedule.courses.map(items => {
+      setCourse(items.subject);
+    });
+
+    const times = schedule.sessions.map(items => {
+      setTime(items.start_time);
+    });
+
     let listData;
     switch (value.date()) {
       case 8:
-        listData = [
-          {
-            type: 'warning',
-            content: `Class: ${schedule.courses[0].subject} Time: ${schedule.schedules[0].start_time}`,
-          },
-          {
-            type: 'success',
-            content: `Class: ${schedule.courses[1].subject} Time: ${schedule.schedules[1].start_time}`,
-          },
-        ];
+        listData = [{ type: 'success', content: [course + ' ' + time] }];
         break;
       case 10:
-        listData = [
-          {
-            type: 'warning',
-            content: `Class: ${schedule.courses[2].subject} Time: ${schedule.schedules[2].start_time}`,
-          },
-          {
-            type: 'success',
-            content: `Class: ${schedule.courses[0].subject} Time: ${schedule.schedules[0].start_time}`,
-          },
-          {
-            type: 'error',
-            content: `Class: ${schedule.courses[1].subject} Time: ${schedule.schedules[1].start_time}`,
-          },
-        ];
+        listData = [{ type: 'success', content: [course + ' ' + time] }];
         break;
       case 15:
-        listData = [
-          {
-            type: 'warning',
-            content: `Class: ${schedule.courses[2].subject} Time: ${schedule.schedules[2].start_time}`,
-          },
-          {
-            type: 'success',
-            content: `Class: ${schedule.courses[0].subject} Time: ${schedule.schedules[0].start_time}`,
-          },
-          {
-            type: 'error',
-            content: `Class: ${schedule.courses[1].subject} Time: ${schedule.schedules[1].start_time}`,
-          },
-          {
-            type: 'error',
-            content: `Class: ${schedule.courses[2].subject} Time: ${schedule.schedules[2].start_time}`,
-          },
-          {
-            type: 'error',
-            content: `Class: ${schedule.courses[0].subject} Time: ${schedule.schedules[0].start_time}`,
-          },
-          {
-            type: 'error',
-            content: `Class: ${schedule.courses[1].subject} Time: ${schedule.schedules[1].start_time}`,
-          },
-        ];
+        listData = [{ type: 'success', content: [course + ' ' + time] }];
         break;
       default:
     }
@@ -108,4 +73,10 @@ function ParentCalendar(props) {
   );
 }
 
-export default ParentCalendar;
+const mapStateToProps = state => {
+  return {
+    schedule: state.parentReducer,
+  };
+};
+
+export default connect(mapStateToProps)(ParentCalendar);

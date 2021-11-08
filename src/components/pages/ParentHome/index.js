619301202cc6363e05020797c2c3ea994fe4dd24
cn8817 <christinenguyen1433@gmail.com>
2021-11-08 13:16:23 -0800

    Added a button that routes to a booking page and aligned components to fit the layout

--- src/components/pages/ParentHome/index.js
+++ src/components/pages/ParentHome/index.js
@@ -3,14 +3,31 @@ import ChildrenList from './ChildrenList';
 import ParentCalendar from './ParentCalendar';
 import ParentSidebar from './ParentSidebar';
 import { dummyData } from '../../../dummyData';
+import Button from '../../common/Button';
+import { useHistory } from 'react-router';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
 
 function ParentHome() {
+  const { Content, Sider } = Layout;
   const [scheduleData, setScheduleData] = useState(dummyData);
+  const { push } = useHistory();
+
+  const handleClick = () => {
+    push('/');
+  };
   return (
     <div className="App">
-      <ChildrenList />
-      <ParentCalendar schedule={scheduleData}></ParentCalendar>
-      <ParentSidebar />
+      <Layout>
+        <Sider width={250}>
+          <ParentSidebar />
+        </Sider>
+        <Content style={{ padding: '15px' }}>
+          <Button buttonText={'Booking'} handleClick={handleClick} />
+          <ParentCalendar schedule={scheduleData} />
+          <ChildrenList />
+        </Content>
+      </Layout>
     </div>
   );
 }

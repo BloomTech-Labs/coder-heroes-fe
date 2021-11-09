feb80f8d2c56541f3099bc66d22a69266d2a7e54
cn8817 <christinenguyen1433@gmail.com>
2021-11-09 13:49:09 -0800

    Added icons to the parent sidebar and made it collapsible

--- src/components/pages/ParentHome/ParentSidebar.js
+++ src/components/pages/ParentHome/ParentSidebar.js
@@ -1,30 +1,82 @@
-import React from 'react';
-import { Menu } from 'antd';
+import React, { useState } from 'react';
+import { Menu, Button, Layout } from 'antd';
 import 'antd/dist/antd.css';
-
+import {
+  TeamOutlined,
+  HomeOutlined,
+  CalendarOutlined,
+  MailOutlined,
+  SettingOutlined,
+  ThunderboltFilled,
+} from '@ant-design/icons';
 //const { SubMenu } = Menu; if submenu is needed in the sidebar
 
+const { Sider } = Layout;
+
 function ParentSidebar() {
+  const [collapsed, setCollapsed] = useState(false);
+  // const [isExpanded, setIsExpanded] = useState(collapsed ? false :true)
+
   const handleClick = e => {
     console.log('click ', e);
   };
 
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+    console.log(collapsed);
+  };
+
   return (
-    <Menu
-      onClick={handleClick}
-      defaultSelectedKeys={['1']}
-      defaultOpenKeys={['sub1']}
-      mode="inline"
-      theme="dark"
-    >
-      <Menu.Item key="1">Overview</Menu.Item>
-      <Menu.Item key="2">Progress</Menu.Item>
-      <Menu.Item key="3">Achievements</Menu.Item>
-      <Menu.Item key="4">Student's Tasks</Menu.Item>
-      <Menu.Item key="5">Resources</Menu.Item>
-      <Menu.Item key="6">Messaging</Menu.Item>
-      <Menu.Item key="7">Booking</Menu.Item>
-    </Menu>
+    <div>
+      {/* <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
+        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
+      </Button> */}
+      <Layout style={{ width: '100%' }}>
+        <Sider
+          style={{ position: 'fixed' }}
+          collapsible
+          collapsed={collapsed}
+          onCollapse={toggleCollapsed}
+        >
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <ThunderboltFilled
+              style={{
+                textAlign: 'center',
+                color: 'white',
+                fontSize: '5rem',
+                padding: '25px 0px',
+              }}
+            />
+          </div>
+          <Menu
+            onClick={handleClick}
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100vh' }}
+            mode="inline"
+            theme="dark"
+            disabledOverflow={true}
+            inlineCollapsed={collapsed}
+          >
+            <Menu.Item key="1" icon={<HomeOutlined />}>
+              Dashboard
+            </Menu.Item>
+            <Menu.Item key="2" icon={<TeamOutlined />}>
+              Family
+            </Menu.Item>
+            <Menu.Item key="3" icon={<CalendarOutlined />}>
+              Schedule
+            </Menu.Item>
+            <Menu.Item key="4" icon={<MailOutlined />}>
+              Inbox
+            </Menu.Item>
+            <Menu.Item key="5" icon={<SettingOutlined />}>
+              Settings
+            </Menu.Item>
+          </Menu>
+        </Sider>
+      </Layout>
+    </div>
   );
 }
 export default ParentSidebar;

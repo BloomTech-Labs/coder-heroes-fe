3cbb8fd467cede065bc53b9e81b63e597195d29c
cn8817 <christinenguyen1433@gmail.com>
2021-11-09 14:24:44 -0800

    Cleaned up parentSidebar and moved code over to the parentHome index.js

--- src/components/pages/ParentHome/ParentSidebar.js
+++ src/components/pages/ParentHome/ParentSidebar.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Menu, Button, Layout } from 'antd';
+import React from 'react';
+import { Menu } from 'antd';
 import 'antd/dist/antd.css';
 import {
   TeamOutlined,
@@ -7,75 +7,41 @@ import {
   CalendarOutlined,
   MailOutlined,
   SettingOutlined,
-  ThunderboltFilled,
 } from '@ant-design/icons';
-//const { SubMenu } = Menu; if submenu is needed in the sidebar
 
-const { Sider } = Layout;
-
-function ParentSidebar() {
-  const [collapsed, setCollapsed] = useState(false);
-  // const [isExpanded, setIsExpanded] = useState(collapsed ? false :true)
-
+function ParentSidebar(props) {
+  const { collapsed } = props;
   const handleClick = e => {
     console.log('click ', e);
   };
 
-  const toggleCollapsed = () => {
-    setCollapsed(!collapsed);
-    console.log(collapsed);
-  };
-
   return (
     <div>
-      {/* <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
-        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
-      </Button> */}
-      <Layout style={{ width: '100%' }}>
-        <Sider
-          style={{ position: 'fixed' }}
-          collapsible
-          collapsed={collapsed}
-          onCollapse={toggleCollapsed}
-        >
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <ThunderboltFilled
-              style={{
-                textAlign: 'center',
-                color: 'white',
-                fontSize: '5rem',
-                padding: '25px 0px',
-              }}
-            />
-          </div>
-          <Menu
-            onClick={handleClick}
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{ height: '100vh' }}
-            mode="inline"
-            theme="dark"
-            disabledOverflow={true}
-            inlineCollapsed={collapsed}
-          >
-            <Menu.Item key="1" icon={<HomeOutlined />}>
-              Dashboard
-            </Menu.Item>
-            <Menu.Item key="2" icon={<TeamOutlined />}>
-              Family
-            </Menu.Item>
-            <Menu.Item key="3" icon={<CalendarOutlined />}>
-              Schedule
-            </Menu.Item>
-            <Menu.Item key="4" icon={<MailOutlined />}>
-              Inbox
-            </Menu.Item>
-            <Menu.Item key="5" icon={<SettingOutlined />}>
-              Settings
-            </Menu.Item>
-          </Menu>
-        </Sider>
-      </Layout>
+      <Menu
+        onClick={handleClick}
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        style={{ height: '100vh' }}
+        mode="inline"
+        theme="dark"
+        inlineCollapsed={collapsed}
+      >
+        <Menu.Item key="1" icon={<HomeOutlined />}>
+          Dashboard
+        </Menu.Item>
+        <Menu.Item key="2" icon={<TeamOutlined />}>
+          Family
+        </Menu.Item>
+        <Menu.Item key="3" icon={<CalendarOutlined />}>
+          Schedule
+        </Menu.Item>
+        <Menu.Item key="4" icon={<MailOutlined />}>
+          Inbox
+        </Menu.Item>
+        <Menu.Item key="5" icon={<SettingOutlined />}>
+          Settings
+        </Menu.Item>
+      </Menu>
     </div>
   );
 }
--- src/components/pages/ParentHome/index.js
+++ src/components/pages/ParentHome/index.js
@@ -7,22 +7,39 @@ import Button from '../../common/Button';
 import { useHistory } from 'react-router';
 import { Layout } from 'antd';
 import 'antd/dist/antd.css';
+import { ThunderboltFilled } from '@ant-design/icons';
 
 function ParentHome() {
   const { Content, Sider } = Layout;
+  const [collapsed, setCollapsed] = useState(false);
   const [scheduleData, setScheduleData] = useState(dummyData);
   const { push } = useHistory();
-
   const handleClick = () => {
     push('/');
   };
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+    console.log(collapsed);
+  };
+
   return (
     <div className="App">
-      <Layout>
-        <Sider width={250}>
+      <Layout style={{ width: '100%' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={toggleCollapsed}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <ThunderboltFilled
+              style={{
+                textAlign: 'center',
+                color: 'white',
+                fontSize: '5rem',
+                padding: '25px 0px',
+              }}
+            />
+          </div>
           <ParentSidebar />
         </Sider>
-        <Content style={{ padding: '15px' }}>
+        <Content>
           <Button buttonText={'Booking'} handleClick={handleClick} />
           <ParentCalendar schedule={scheduleData} />
           <ChildrenList />

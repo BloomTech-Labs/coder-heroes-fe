7fcbd5f6e9b7cf640f66dec9d72ac4583025427f
Fernando Martinez <fernando817mm@gmail.com>
2021-11-09 13:32:06 -0600

    Added parent rescources and styled the structure of the parent dashboard

---
+++ src/components/pages/ParentHome/ParentResources.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ParentResources = props => {
+  return (
+    <>
+      <h1>Parent Resources</h1>
+      <div></div>
+    </>
+  );
+};
+
+export default ParentResources;
--- src/components/pages/ParentHome/index.js
+++ src/components/pages/ParentHome/index.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import ChildrenList from './ChildrenList';
 import ParentCalendar from './ParentCalendar';
 import ParentSidebar from './ParentSidebar';
+import ParentResources from './ParentResources';
 import { dummyData } from '../../../dummyData';
 import Button from '../../common/Button';
 import { useHistory } from 'react-router';
-import { Layout } from 'antd';
+import { Layout, Row, Col } from 'antd';
 import 'antd/dist/antd.css';
+import './../../../styles/ParentStyles/index.less';
 
 function ParentHome() {
   const { Content, Sider } = Layout;
@@ -19,12 +21,23 @@ function ParentHome() {
   return (
     <div className="App">
       <Layout>
-        <Sider width={250}>
+        <Sider width={150}>
           <ParentSidebar />
         </Sider>
         <Content style={{ padding: '15px' }}>
           <Button buttonText={'Booking'} handleClick={handleClick} />
-          <ParentCalendar schedule={scheduleData} />
+          <Layout>
+            <Content>
+              <Row justify="space-around" align="middle">
+                <Col span={5}>
+                  <ParentResources />
+                </Col>
+                <Col span={15}>
+                  <ParentCalendar schedule={scheduleData} />
+                </Col>
+              </Row>
+            </Content>
+          </Layout>
           <ChildrenList />
         </Content>
       </Layout>

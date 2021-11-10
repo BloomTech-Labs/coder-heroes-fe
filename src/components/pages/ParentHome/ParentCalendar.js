814a11f696c39323a5eb52176c49f2f7e900203c
muhannadbm <mhd31145@hotmail.com>
2021-11-10 11:34:25 -0500

    added some stuff to parent calender

--- src/components/pages/ParentHome/ParentCalendar.js
+++ src/components/pages/ParentHome/ParentCalendar.js
@@ -1,39 +1,52 @@
 import React, { useState } from 'react';
-
+import '../../../styles/ParentStyles/index.less';
 import 'antd/dist/antd.css';
 
 import { Calendar, Badge, Modal } from 'antd';
 
 import { connect } from 'react-redux';
+import { useEffect } from 'react';
 
 function ParentCalendar(props) {
   const { schedule } = props;
   const [course, setCourse] = useState('');
   const [time, setTime] = useState('');
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [activeModal, setActiveModal] = useState(0);
+
+  // console.log(schedule)
 
   function getListData(value) {
     const values = schedule.courses.map(items => {
-      setCourse(items.subject);
+      setCourse(items);
     });
 
-    const times = schedule.sessions.map(items => {
-      setTime(items.start_time);
-    });
+    let arr = [];
 
     let listData;
-    switch (value.date()) {
-      case 8:
-        listData = [{ type: 'success', content: [course + ' ' + time] }];
-        break;
-      case 10:
-        listData = [{ type: 'success', content: [course + ' ' + time] }];
-        break;
-      case 15:
-        listData = [{ type: 'success', content: [course + ' ' + time] }];
-        break;
-      default:
-    }
+    let today = new Date().toLocaleString('en-US', {
+      day: '2-digit', // numeric, 2-digit
+      year: 'numeric', // numeric, 2-digit
+      month: '2-digit', // numeric, 2-digit, long, short, narrow
+    });
+
+    schedule.sessions.forEach(item => {
+      let new_startDate = item.start_date.replaceAll('-', '/');
+      // let new_endDate = item.end_date.replaceAll('-', '/')
+      switch (value.format('L')) {
+        case new_startDate:
+          listData = [
+            {
+              type: today === value.format('L') ? 'success' : 'warning',
+              content:
+                item.course + ' ' + item.start_time + ' - ' + item.end_time,
+            },
+          ];
+          break;
+        default:
+      }
+    });
+
     return listData || [];
   }
 
@@ -41,35 +54,57 @@ function ParentCalendar(props) {
   //   console.log(e.target.index);
   // };
 
-  const showModal = () => {
-    setIsModalVisible(true);
+  const showModal = id => {
+    setActiveModal(id);
+    // setIsModalVisible(true);
   };
 
   const handleOk = () => {
+    setActiveModal(0);
     setIsModalVisible(false);
   };
 
   const handleCancel = () => {
+    setActiveModal(0);
     setIsModalVisible(false);
   };
 
   function dateCellRender(value) {
     const listData = getListData(value);
+    let today = new Date().toLocaleString('en-US', {
+      day: '2-digit', // numeric, 2-digit
+      year: 'numeric', // numeric, 2-digit
+      month: '2-digit', // numeric, 2-digit, long, short, narrow
+    });
     return (
       <ul className="events">
-        {listData.map((item, idx) => (
-          <li key={item.content}>
-            <Badge status={item.type} text={item.content} onClick={showModal} />
-            <Modal
-              title={item.content}
-              visible={isModalVisible}
-              onOk={handleOk}
-              onCancel={handleCancel}
-            >
-              <p>{item.content}</p>
-            </Modal>
-          </li>
-        ))}
+        {listData.map(item => {
+          return (
+            <li className="cell" key={item.schedule_id}>
+              {today === value.format('L') ? (
+                <div className="today"> Today</div>
+              ) : null}
+              {console.log('inside badge', item.content)}
+              <Badge
+                className="badge"
+                status={item.type}
+                text={item.content}
+                onClick={e => showModal(item.schedule_id)}
+              />
+              {console.log('inside Modal', item.content)}
+              <Modal
+                className="events"
+                title={'my Title'}
+                visible={item.schedule_id === activeModal ? true : false}
+                onOk={handleOk}
+                onCancel={handleCancel}
+              >
+                <p>{item.content}</p>
+                <p>{item.schedule_id}</p>
+              </Modal>
+            </li>
+          );
+        })}
       </ul>
     );
   }
@@ -83,7 +118,7 @@ function ParentCalendar(props) {
   function monthCellRender(value) {
     const num = getMonthData(value);
     return num ? (
-      <div className="notes-month">
+      <div className="events">
         <section>{num}</section>
         <span>Backlog number</span>
       </div>
@@ -91,10 +126,18 @@ function ParentCalendar(props) {
   }
 
   return (
-    <Calendar
-      dateCellRender={dateCellRender}
-      monthCellRender={monthCellRender}
-    />
+    <>
+      {/* {console.log(time)}
+    {console.log(schedule)} */}
+      <Calendar
+        className="events"
+        // onChange={(value) => {
+        //   alert(`Your selected ${value.format('YYYY-MM-DD')}`)
+        // }}
+        dateCellRender={dateCellRender}
+        monthCellRender={monthCellRender}
+      />{' '}
+    </>
   );
 }
 

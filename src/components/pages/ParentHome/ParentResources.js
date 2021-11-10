f0743e2af43192ee273b7038d251c967687c809d
Fernando Martinez <fernando817mm@gmail.com>
2021-11-10 16:29:01 -0600

    Added static data and temporary in-line styling to ParentResources.js

--- src/components/pages/ParentHome/ParentResources.js
+++ src/components/pages/ParentHome/ParentResources.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import 'antd/dist/antd.css';
+import { Space, Card } from 'antd';
+import { MailOutlined } from '@ant-design/icons';
 
 const ParentResources = props => {
   return (
-    <>
-      <h1>Parent Resources</h1>
-      <div></div>
-    </>
+    <div>
+      <h2 style={{ fontSize: '2.5rem', textAlign: 'center' }}>
+        {<MailOutlined />} News Feed
+      </h2>
+      <Space direction="vertical" style={{ width: '100%' }}>
+        <Card title="Mrs. Rodriguez" style={{ width: '100%' }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card title="Mrs. Smith" style={{ width: '100%' }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card title="Mrs. Lewis" style={{ width: '100%' }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+      </Space>
+    </div>
   );
 };
 

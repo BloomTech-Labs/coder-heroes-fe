b4bacec152b3b6b7f3937409ab41d0e785babf49
ehsan2793 <ehsan2793@gmail.com>
2021-12-03 09:26:16 -0800

    removed the eslint error by eslint disable

--- src/components/pages/ParentHome/AvailableCourses.js
+++ src/components/pages/ParentHome/AvailableCourses.js
@@ -10,6 +10,7 @@ const AvailableCourses = props => {
   const [currentTab, SetcurrentTab] = useState();
   useEffect(() => {
     SetcurrentTab(courses.sessions);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   let Datetoday = new Date();
   let week = new Date();

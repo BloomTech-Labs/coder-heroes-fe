5b4bd33ceb176f561746fea2b2ad729d48a871b2
TraNequaFauntleroy <shecodes@nenethecreative.com>
2021-12-12 18:48:46 -0500

    playing around with collasping the sidebar

--- src/components/pages/ParentHome/ParentSidebar.js
+++ src/components/pages/ParentHome/ParentSidebar.js
@@ -19,6 +19,18 @@ function ParentSidebar(props) {
     console.log('click ', e);
   };
 
+  /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+  function openNav() {
+    document.getElementById('mySidebar').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+  }
+
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById('mySidebar').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+  }
+
   return (
     <div>
       <Menu
@@ -32,37 +44,49 @@ function ParentSidebar(props) {
         color="orange"
         inlineCollapsed={collapsed}
       >
+        {/* <a href="javascript:void(0)" className="closebtn" onclick="closeNav()">&times;</a> */}
+
+        <Menu.Item key="2" className="closebtn" onclick="closeNav()">
+          X
+        </Menu.Item>
         <Menu.Item
           key="1"
           className="dashboard-logo"
-          icon={<ThunderboltFilled />}
+          icon={<ThunderboltFilled style={{ fontSize: '150%' }} />}
         >
+          {' '}
           CoderHeroes
         </Menu.Item>
-        <Menu.Item key="2" icon={<HomeFilled />}>
+        <Menu.Item key="2" icon={<HomeFilled style={{ fontSize: '150%' }} />}>
           <Link to="/parent" className="link">
             Dashboard
           </Link>
         </Menu.Item>
-        <Menu.Item key="4" icon={<HeartFilled />}>
+        <Menu.Item key="3" icon={<HeartFilled style={{ fontSize: '150%' }} />}>
           <Link to="/" className="link">
             {' '}
             ** Family
           </Link>
         </Menu.Item>
-        <Menu.Item key="3" icon={<CalendarFilled />}>
+        <Menu.Item
+          key="4"
+          icon={<CalendarFilled style={{ fontSize: '150%' }} />}
+        >
           <Link to="/parent-booking" className="link">
             Booking
           </Link>
         </Menu.Item>
-        <Menu.Item key="4" icon={<ToolFilled />}>
+        <Menu.Item key="5" icon={<ToolFilled style={{ fontSize: '150%' }} />}>
           <Link to="/settings" className="link">
             {' '}
             ** Settings
           </Link>
         </Menu.Item>
 
-        <Menu.Item key="4" icon={<ExportOutlined />}>
+        <Menu.Item
+          key="4"
+          icon={<ExportOutlined style={{ fontSize: '150%' }} />}
+        >
           <Link to="/logout" className="link">
             {' '}
             ** Logout

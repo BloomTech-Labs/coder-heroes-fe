f73163fb98e7680f894739f08bb7ce8e96616d27
TraNequaFauntleroy <shecodes@nenethecreative.com>
2021-12-13 11:54:55 -0500

    a little code cleanup

--- src/components/pages/ParentHome/ParentSidebar.js
+++ src/components/pages/ParentHome/ParentSidebar.js
@@ -21,13 +21,13 @@ function ParentSidebar(props) {
 
   /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
   function openNav() {
-    document.getElementById('mySidebar').style.width = '250px';
+    document.getElementById('parent-dashboard-sidebar').style.width = '250px';
     document.getElementById('main').style.marginLeft = '250px';
   }
 
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
-    document.getElementById('mySidebar').style.width = '0';
+    document.getElementById('parent-dashboard-sidebar').style.width = '0';
     document.getElementById('main').style.marginLeft = '0';
   }
 

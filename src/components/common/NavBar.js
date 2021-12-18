c4a3e353ab41edc863d6136e041cb40fa2a4cc0c
BrendonHawkins <artbitcodedata@gmail.com>
2021-12-18 17:36:59 -0500

    beginning stages of Nav Skeleton⻤

---
+++ src/components/common/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function MainNav() {
+  return (
+    <nav className="mainNav">
+      <div className="nav navLogo"></div>
+      <div className="nav navOptions"></div>
+      <div className="nav navSocial"></div>
+      <div className="nav navContact"></div>
+      <div className="nav navSignup"></div>
+    </nav>
+  );
+}

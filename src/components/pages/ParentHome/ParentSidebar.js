37e894f8c5c31f14b77f9ca176645b24c741a2fe
Ryan Mark <rymark907@gmail.com>
2022-01-13 13:00:36 -0800

    removed the duplicate buttons on the sidebar for the parent dashboard

--- src/components/pages/ParentHome/ParentSidebar.js
+++ src/components/pages/ParentHome/ParentSidebar.js
@@ -75,22 +75,10 @@ const ParentSideBar = props => {
           <Link to="/logout">Logout</Link>
         </Menu.Item>
 
-        <Menu.Item key="family" icon={<HeartFilled fontSize="150px" />}>
-          <Link to="/family">Family</Link>
-        </Menu.Item>
-
-        <Menu.Item key="setting" icon={<ToolFilled fontSize="150px" />}>
-          <Link to="/settings">Settings</Link>
-        </Menu.Item>
-
         <Menu.Item key="4" icon={<ShoppingCartOutlined fontSize="150px" />}>
           <Link to="/cart" className="link">
             Cart <span>({cart.length})</span>
           </Link>
-        </Menu.Item>
-
-        <Menu.Item key="logout" icon={<ExportOutlined fontSize="150px" />}>
-          <Link to="/logout">Logout</Link>
         </Menu.Item>
       </Menu>
     </Sider>

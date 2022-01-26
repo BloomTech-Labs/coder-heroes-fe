73ad80e3b30f4ee46ffd95815e60e979362ac51e
Jaehun ParkLaptop <pjaehun4122@gmail.com>
2022-01-26 14:35:06 -0800

    changing a tag to Link instead

--- src/components/pages/ParentHome/ParentSidebar.js
+++ src/components/pages/ParentHome/ParentSidebar.js
@@ -64,7 +64,7 @@ const ParentSideBar = props => {
           <Link to="/family">Family</Link>
         </Menu.Item>
         <Menu.Item key="3" icon={<ReadOutlined />}>
-          <a href="/parent-news-feed">News Feed</a>
+          <Link to="/parent-news-feed">News Feed</Link>
         </Menu.Item>
 
         <Menu.Item key="setting" icon={<ToolFilled fontSize="150px" />}>

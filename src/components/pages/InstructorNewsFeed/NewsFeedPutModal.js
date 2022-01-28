2da178e14b1a06567bbbd04b81779f3f891c9656
Jaehun ParkLaptop <pjaehun4122@gmail.com>
2022-01-28 10:16:16 -0800

    trying to add getauthheader function

--- src/components/pages/InstructorNewsFeed/NewsFeedPutModal.js
+++ src/components/pages/InstructorNewsFeed/NewsFeedPutModal.js
@@ -1,35 +1,39 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Input, Button } from 'antd';
 import axios from 'axios';
-import '../../../styles/InstructorStyles/index.less';
+import '../../../styles/index.less';
 import { CloseOutlined } from '@ant-design/icons';
+import { useOktaAuth } from '@okta/okta-react';
 export default function NewsfeedPutModal(props) {
+  const { authState } = useOktaAuth();
+  console.log('this is auth state',authState);
   const { setPostOptions, postID } = props;
-  const token = JSON.parse(localStorage.getItem('okta-token-storage'));
-  const config = {
-    headers: { Authorization: `Bearer ${token.idToken.value}` },
-  };
+  const token = authState.idToken;
+  console.log(typeof token);
+  // const config = {
+  //   headers: { Authorization: `Bearer ${token.idToken.value}` },
+  // };
   const [formValues, setFormValues] = useState({
     link: '',
     description: '',
     title: '',
   });
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     axios
-      .get(`https://coder-heroes-api.herokuapp.com/news/${postID}`, config)
+      .get(`https://coder-heroes-api.herokuapp.com/news/${postID}`,token)
       .then(resp => {
         setFormValues({
           ...formValues,
-          link:resp.data.link,
-          description:resp.data.description,
-          title: resp.data.title
+          link: resp.data.link,
+          description: resp.data.description,
+          title: resp.data.title,
         });
       })
       .catch(err => {
         console.error(err);
       });
-  },[]);
+  }, []);
   const handleChange = e => {
     setFormValues({
       ...formValues,
@@ -42,7 +46,7 @@ export default function NewsfeedPutModal(props) {
       .put(
         `https://coder-heroes-api.herokuapp.com/news/${postID}`,
         formValues,
-        config
+        token
       )
       .then(resp => {
         setPostOptions('newsFeed');
@@ -53,7 +57,7 @@ export default function NewsfeedPutModal(props) {
   };
   const handleDelete = () => {
     axios
-      .delete(`https://coder-heroes-api.herokuapp.com/news/${postID}`, config)
+      .delete(`https://coder-heroes-api.herokuapp.com/news/${postID}`, token)
       .then(resp => {
         setPostOptions('newsFeed');
       })
@@ -66,22 +70,31 @@ export default function NewsfeedPutModal(props) {
     <div key={postID} className="newsfeedForm_container">
       <div className="newsfeedForm_header">
         <h1>Edit/Delete Post</h1>
-        <CloseOutlined  
+        <CloseOutlined
           onClick={() => {
             setPostOptions('newsFeed');
-          }}/>
+          }}
+        />
       </div>
       <Form>
         <div className="newsfeedForm_input_container">
           <Form.Item label="Post Title:">
-            <Input name="title" value={formValues.title} onChange={handleChange} />
+            <Input
+              name="title"
+              value={formValues.title}
+              onChange={handleChange}
+            />
           </Form.Item>
-          <Form.Item  label="Link:">
-            <Input name="link" value={formValues.link} onChange={handleChange} />
+          <Form.Item label="Link:">
+            <Input
+              name="link"
+              value={formValues.link}
+              onChange={handleChange}
+            />
           </Form.Item>
         </div>
         <div className="newsfeedForm_inputfield">
-          <Form.Item  label="Post Contents:" />
+          <Form.Item label="Post Contents:" />
           <Input.TextArea
             name="description"
             onChange={handleChange}

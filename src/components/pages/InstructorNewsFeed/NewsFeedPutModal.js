bf70d84e7b6e1cc604ad2b6722a703558206c513
Jaehun ParkLaptop <pjaehun4122@gmail.com>
2022-02-02 12:59:50 -0800

    axiosWithAuth implemented, form validation for newsfeed post request implemented, style change that give font to disabled submit buttons, left comments for the code that need to be edited later on

--- src/components/pages/InstructorNewsFeed/NewsFeedPutModal.js
+++ src/components/pages/InstructorNewsFeed/NewsFeedPutModal.js
@@ -1,26 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Input, Button } from 'antd';
-import axios from 'axios';
+import axios from 'axios'; // (this line of code can be deleted after PR130 get merged to the main branch)
 import '../../../styles/index.less';
 import { CloseOutlined } from '@ant-design/icons';
-import { useOktaAuth } from '@okta/okta-react';
-import { getAuthHeader } from '../../../api/index';
+import { useOktaAuth } from '@okta/okta-react'; // (this line of code can be deleted after PR130 get merged to the main branch)
+import { getAuthHeader } from '../../../api/index'; // (this line of code can be deleted after PR130 get merged to the main branch)
+import axiosWithAuth from '../../../utils/axiosWithAuth';// (this code will be usable after PR130 get merged to the main branch)
 
 export default function NewsfeedPutModal(props) {
-  const { authState } = useOktaAuth();
   const { setPostOptions, postID } = props;
-  const token = {
-    headers: getAuthHeader(authState),
-  };
+  // (below code can be deleted after PR130 get merged to the main branch)
+  // const { authState } = useOktaAuth();
+  // const token = {
+  //   headers: getAuthHeader(authState),
+  // };
+   // (above code can be deleted after PR130 get merged to the main branch)
   const [formValues, setFormValues] = useState({
     link: '',
     description: '',
     title: '',
   });
 
+  // (this code will be usable after PR130 get merged to the main branch)
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_API_URI}/news/${postID}`, token)
+    axiosWithAuth()
+      .get(`/news/${postID}`)
       .then(resp => {
         setFormValues({
           ...formValues,
@@ -33,16 +37,36 @@ export default function NewsfeedPutModal(props) {
         console.error(err);
       });
   }, []);
+  // (this code will be usable after PR130 get merged to the main branch)
+
+
+  // (below code can be deleted after PR130 get merged to the main branch)
+  // useEffect(() => {
+  //   axios
+  //     .get(`${process.env.REACT_APP_API_URI}/news/${postID}`, token)
+  //     .then(resp => {
+  //       setFormValues({
+  //         ...formValues,
+  //         link: resp.data.link,
+  //         description: resp.data.description,
+  //         title: resp.data.title,
+  //       });
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     });
+  // }, []);
+  // (above code can be deleted after PR130 get merged to the main branch)
+  
   const handleChange = e => {
     setFormValues({
       ...formValues,
       [e.target.name]: e.target.value,
     });
   };
-
   const handleEdit = () => {
-    axios
-      .put(`${process.env.REACT_APP_API_URI}news/${postID}`, formValues, token)
+    axiosWithAuth()
+      .put(`/news/${postID}`, formValues)
       .then(resp => {
         setPostOptions('newsFeed');
       })
@@ -51,8 +75,8 @@ export default function NewsfeedPutModal(props) {
       });
   };
   const handleDelete = () => {
-    axios
-      .delete(`${process.env.REACT_APP_API_URI}news/${postID}`, token)
+    axiosWithAuth()
+      .delete(`/news/${postID}`)
       .then(resp => {
         setPostOptions('newsFeed');
       })
@@ -60,7 +84,28 @@ export default function NewsfeedPutModal(props) {
         console.log(err);
       });
   };
-
+   // (below code can be deleted after PR130 get merged to the main branch)
+  // const handleEdit = () => {
+  //   axios
+  //     .put(`${process.env.REACT_APP_API_URI}news/${postID}`, formValues, token)
+  //     .then(resp => {
+  //       setPostOptions('newsFeed');
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // };
+  // const handleDelete = () => {
+  //   axios
+  //     .delete(`${process.env.REACT_APP_API_URI}news/${postID}`, token)
+  //     .then(resp => {
+  //       setPostOptions('newsFeed');
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // };
+   // (above code can be deleted after PR130 get merged to the main branch)
   return (
     <div key={postID} className="newsfeedForm_container">
       <div className="newsfeedForm_header">

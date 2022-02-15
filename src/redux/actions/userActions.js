116ce06eb24e36afbbc7f04a464b904ee40ee79d
Kristian Fulkerson <kristianf451@gmail.com>
2022-02-14 16:08:52 -0800

    dispatch getCurrentUser onmount of landing page if okta token exists in storage

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,11 +1,12 @@
 import axiosWithAuth from '../../utils/axiosWithAuth';
 export const GET_CURRENT_USER = 'GET_CURRENT_USER';
 
+//thunk action
 export const getCurrentUser = oktaID => dispatch => {
   axiosWithAuth()
     .get(`/profiles/${oktaID}`)
     .then(res => {
-      console.log(res);
+      dispatch({ type: GET_CURRENT_USER, payload: res.data });
     })
     .catch(err => {
       console.error(err.message);

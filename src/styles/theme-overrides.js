d5a6583a724e1759917086b33f84ca0335e77401
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-02-20 21:56:28 -0800

    feat: border and box-shadow input effect

--- src/styles/theme-overrides.js
+++ src/styles/theme-overrides.js
@@ -4,19 +4,24 @@ module.exports = {
   '@success-color': '#52c41a',
   '@warning-color': '#faad14',
   '@error-color': '#f5222d',
+  // Font
   '@font-size-base': '14px',
   '@heading-color': 'rgba(0, 0, 0, 0.85)',
   '@text-color': 'rgba(0, 0, 0, 0.65)',
   '@text-color-secondary': 'rgba(0, 0, 0, 0.45)',
+  // Effects
   '@disabled-color': 'rgba(0, 0, 0, 0.25)',
   '@border-radius-base': '2px',
   '@border-color-base': '#d9d9d9',
   '@box-shadow-base':
     '0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 9px 28px 8px rgba(0, 0, 0, 0.05)',
-  // Background Colors
+  // Background
   '@bg-color': '#eeedd9',
   '@bg-footer-color': '#6a0c49',
   '@bg-header-color': '#fead2a',
-  // Button Colors
-  '@secondary-btn-bg-color': '#fead2a',
+  // Button
+  '@secondary-btn-bg-color': '#f2ce60',
+  // Inputs
+  '@input-focused-border': '2px solid #21c5b5',
+  '@input-focused-box-shadow': '0px 0px 12px 1px #21c5b5',
 };

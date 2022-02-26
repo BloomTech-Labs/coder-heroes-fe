47632b28b8fa532db14097982a4e60dfd49cd721
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-02-25 18:24:04 -0800

    feat: create theme file and reset theme override
    Co-authored-by: priscila monteiro <pririmonteiro@me.com>
    Co-authored-by: moduped <daniemu@mail.uc.edu>

---
+++ src/styles/theme/coderHeroesTheme.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // Backgrounds
+  '@header__bg': '#FEAD2A',
+  '@footer__bg': '#6A0C49',
+  '@body__bg': '#EEEDD9',
+  '@body__bg--green': '#9FB222',
+  '@body__bg--blue': '#096A70',
+  '@body__bg--peach': '#FCDFAA',
+  '@body__bg--concrete': '#F2F2F2',
+  '@body__bg--white': '#FAF9F8',
+  // Buttons
+  '@btn__bg--blue': '#21C5B5',
+  '@btn__bg--white': '#EEEDD9',
+  '@btn__bg--red': '#EC2C4E',
+  '@btn__bg--maroon': '#6A0C49',
+  '@btn__bg--yellow': '#F2CE60',
+  '@btn__bg--orange': '#F66721',
+  // Borders
+  '@btn__border-radius': '6px',
+  // Fonts
+  '@text__font-family': "'Montserrat', sans-serif",
+  '@text__font-family--header': "'Staatliches', cursive",
+  '@text__color--maroon': '#3B0E2B',
+  '@text__color--white': '#FFFFFF',
+  '@text__color--black': '#232420',
+  // Focus Effects
+  '@input__border--focus': '1px solid #21C5B5',
+  '@input__box-shadow--focus': '0px 0px 10px 1px #21C5B5',
+};

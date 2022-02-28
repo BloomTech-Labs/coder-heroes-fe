b765465e970d0b62bcb4042e1c56e12de2238385
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-02-28 08:10:48 -0800

    feat: add Figma text styles to theme file
    Co-authored-by: priscila monteiro <pririmonteiro@me.com>
    Co-authored-by: moduped <daniemu@mail.uc.edu>
    Co-authored-by: Nathan Cheney <natecheney@gmail.com>

--- src/styles/theme/coderHeroesTheme.js
+++ src/styles/theme/coderHeroesTheme.js
@@ -1,7 +1,9 @@
 module.exports = {
   // Backgrounds
   '@header__bg': '#FEAD2A',
+  '@header__bg--dark': '#263E47',
   '@footer__bg': '#6A0C49',
+
   '@body__bg': '#EEEDD9',
   '@body__bg--green': '#9FB222',
   '@body__bg--blue': '#096A70',
@@ -20,10 +22,28 @@ module.exports = {
   // Fonts
   '@text__font-family': "'Montserrat', sans-serif",
   '@text__font-family--header': "'Staatliches', cursive",
+
+  '@text__font-size--h1': '2.375rem',
+  '@text__font-size--h2': '1.875rem',
+  '@text__font-size--h3': '1.5rem',
+  '@text__font-size--h4': '1.25rem',
+  '@text__font-size--h5': '1rem',
+  '@text__font-size--body': '0.875rem',
+  '@text__font-size--footer': '0.75rem',
+
+  '@text__line-height--h1': '2.875rem',
+  '@text__line-height--h2': '2.5rem',
+  '@text__line-height--h3': '2rem',
+  '@text__line-height--h4': '1.75rem',
+  '@text__line-height--h5': '1.5rem',
+  '@text__line-height--body': '1.375rem',
+  '@text__line-height--footer': '1.25rem',
+
   '@text__color--maroon': '#3B0E2B',
   '@text__color--white': '#FFFFFF',
   '@text__color--black': '#232420',
   // Focus Effects
   '@input__border--focus': '1px solid #21C5B5',
   '@input__box-shadow--focus': '0px 0px 10px 1px #21C5B5',
+  '@input__transition--focus': '0.1s',
 };

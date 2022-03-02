3de99e450ba8934d95f5a598d51dca0986fb9406
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-02 15:03:00 -0800

    feat: create new footer container using antd
    Co-authored-by: priscila monteiro <pririmonteiro@me.com>
    Co-authored-by: moduped <daniemu@mail.uc.edu>
    Co-authored-by: Kristian Fulkerson <kristianf451@gmail.com>

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,78 +1,51 @@
 import React from 'react';
-import { Layout } from 'antd';
-import {
-  FacebookOutlined,
-  TwitterOutlined,
-  InstagramOutlined,
-  YoutubeOutlined,
-  LinkedinOutlined,
-} from '@ant-design/icons';
+import { Layout, Row, Col, Typography } from 'antd';
+// The icons bellow will be used to help build the footer
+// import {
+//   FacebookOutlined,
+//   TwitterOutlined,
+//   InstagramOutlined,
+//   YoutubeOutlined,
+//   LinkedinOutlined,
+// } from '@ant-design/icons';
 
 const { Footer } = Layout;
+const { Title } = Typography;
 
-export default function MainFooter({ props }) {
+function MainFooter() {
   return (
-    <div>
-      <Footer id="footer">
-        <div className="footer-container">
-          <div className="footer-section">
-            <h2>Contact Us</h2>
-            <a href="#.">email@email.com</a>
-            <a href="#.">Find a Program</a>
-          </div>
-
-          <div className="footer-section">
-            <h2>Services</h2>
-            <a href="#.">FAQ</a>
-            <a href="#.">Site Map</a>
-          </div>
-
-          <div className="footer-section">
-            <h2>Information</h2>
-            <a href="#.">About Coderheroes</a>
-            <a href="#.">Privacy Policy</a>
-            <a href="#.">Terms and Conditions</a>
-            <a href="#.">Press Inquiries</a>
-          </div>
-
-          <div style={{ textAlign: 'center' }} className="footer-section">
-            <h1>Coderheroes</h1>
-            <div className="footerSocial">
-              <a
-                href="https://www.facebook.com/coderheroes."
-                className="footer-socialIcon"
-              >
-                <FacebookOutlined />
-              </a>
-              <a
-                href="https://twitter.com/coderheroes."
-                className="footer-socialIcon"
-              >
-                <TwitterOutlined />
-              </a>
-              <a
-                href="https://www.instagram.com/coderheroes/."
-                className="footer-socialIcon"
-              >
-                <InstagramOutlined />
-              </a>
-              <a
-                href="https://www.youtube.com/channel/UC7vHHesa12tznVpgvnwbnKg."
-                className="footer-socialIcon"
-              >
-                <YoutubeOutlined />
-              </a>
-              <a
-                href="https://www.linkedin.com/company/coderheroes/."
-                className="footer-socialIcon"
-              >
-                <LinkedinOutlined />
-              </a>
-            </div>
-            <small>Copyright &#169; 2020 coderheroes Inc.</small>
-          </div>
-        </div>
-      </Footer>
-    </div>
+    <Footer style={{ padding: '40px', backgroundColor: '#6A0C49' }}>
+      <Row justify="space-between">
+        <Col className="footer__column">
+          <Title className="title" style={{ color: '#ffffff' }} level={3}>
+            About
+          </Title>
+        </Col>
+        <Col className="footer__column">
+          <Title className="title" style={{ color: '#ffffff' }} level={3}>
+            Services
+          </Title>
+        </Col>
+        <Col className="footer__column">
+          <Title className="title" style={{ color: '#ffffff' }} level={3}>
+            Contact
+          </Title>
+        </Col>
+        <Col className="footer__column" style={{ textAlign: 'right' }}>
+          <Title className="title" style={{ color: '#ffffff' }} level={1}>
+            CoderHeroes
+          </Title>
+        </Col>
+      </Row>
+    </Footer>
   );
 }
+
+// CoderHeroes Social Media Links
+// https://www.facebook.com/coderheroes/
+// https://twitter.com/coderheroes/
+// https://www.instagram.com/coderheroes/
+// https://www.youtube.com/channel/UC7vHHesa12tznVpgvnwbnKg/
+// https://www.linkedin.com/company/coderheroes/
+
+export default MainFooter;

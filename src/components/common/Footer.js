f676c00dbef72b3a4d634b25f330a0f108aa568b
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-02 21:34:22 -0800

    feat: create footer for desktop display
    Co-authored-by: priscila monteiro <pririmonteiro@me.com>
    Co-authored-by: moduped <daniemu@mail.uc.edu>
    Co-authored-by: Kristian Fulkerson <kristianf451@gmail.com>

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,51 +1,183 @@
 import React from 'react';
 import { Layout, Row, Col, Typography } from 'antd';
-// The icons bellow will be used to help build the footer
-// import {
-//   FacebookOutlined,
-//   TwitterOutlined,
-//   InstagramOutlined,
-//   YoutubeOutlined,
-//   LinkedinOutlined,
-// } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import {
+  FacebookOutlined,
+  TwitterOutlined,
+  InstagramOutlined,
+  YoutubeOutlined,
+  LinkedinOutlined,
+} from '@ant-design/icons';
 
 const { Footer } = Layout;
 const { Title } = Typography;
 
 function MainFooter() {
   return (
-    <Footer style={{ padding: '40px', backgroundColor: '#6A0C49' }}>
-      <Row justify="space-between">
-        <Col className="footer__column">
-          <Title className="title" style={{ color: '#ffffff' }} level={3}>
-            About
-          </Title>
-        </Col>
-        <Col className="footer__column">
-          <Title className="title" style={{ color: '#ffffff' }} level={3}>
-            Services
-          </Title>
-        </Col>
-        <Col className="footer__column">
-          <Title className="title" style={{ color: '#ffffff' }} level={3}>
+    <Footer
+      style={{
+        padding: '40px',
+        backgroundColor: '#6A0C49',
+      }}
+    >
+      <Row
+        justify="space-between"
+        style={{ maxWidth: '1100px', margin: '0 auto' }}
+      >
+        <Col>
+          <Title
+            className="title"
+            style={{ color: '#ffffff', fontWeight: '400' }}
+            level={3}
+          >
             Contact
           </Title>
+          <div>
+            <ul>
+              <li className="footer__text">brianne@coderheroes.com</li>
+              <li>
+                <Link className="footer__nav-link footer__text">
+                  Find a Program
+                </Link>
+              </li>
+            </ul>
+          </div>
         </Col>
-        <Col className="footer__column" style={{ textAlign: 'right' }}>
-          <Title className="title" style={{ color: '#ffffff' }} level={1}>
+        <Col>
+          <Title
+            className="title"
+            style={{ color: '#ffffff', fontWeight: '400' }}
+            level={3}
+          >
+            Services
+          </Title>
+          <div>
+            <ul>
+              <li>
+                <Link className="footer__nav-link footer__text">FAQ</Link>
+              </li>
+              <li>
+                <Link className="footer__nav-link footer__text">Site Map</Link>
+              </li>
+            </ul>
+          </div>
+        </Col>
+        <Col>
+          <Title
+            className="title"
+            style={{ color: '#ffffff', fontWeight: '400' }}
+            level={3}
+          >
+            About
+          </Title>
+          <div>
+            <ul>
+              <li>
+                <Link className="footer__nav-link footer__text">
+                  About CoderHeroes
+                </Link>
+              </li>
+              <li>
+                <Link className="footer__nav-link footer__text">
+                  Privacy Policy
+                </Link>
+              </li>
+              <li>
+                <Link className="footer__nav-link footer__text">
+                  Terms and Conditions
+                </Link>
+              </li>
+              <li>
+                <Link className="footer__nav-link footer__text">
+                  Press Inquiries
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </Col>
+        <Col
+          style={{
+            textAlign: 'right',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Title
+            className="title"
+            style={{
+              color: '#ffffff',
+              marginBottom: '0px',
+              fontWeight: '400',
+            }}
+            level={1}
+          >
             CoderHeroes
           </Title>
+          <Row style={{ justifyContent: 'space-between', marginLeft: '20px' }}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.facebook.com/coderheroes/"
+            >
+              <FacebookOutlined
+                style={{
+                  color: '#ffffff',
+                  fontSize: '40px',
+                }}
+              />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://twitter.com/coderheroes/"
+            >
+              <TwitterOutlined style={{ color: '#ffffff', fontSize: '40px' }} />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/coderheroes/"
+            >
+              <InstagramOutlined
+                style={{
+                  color: '#ffffff',
+                  fontSize: '40px',
+                }}
+              />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.youtube.com/channel/UC7vHHesa12tznVpgvnwbnKg/"
+            >
+              <YoutubeOutlined
+                style={{
+                  color: '#ffffff',
+                  fontSize: '40px',
+                }}
+              />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/company/coderheroes/"
+            >
+              <LinkedinOutlined
+                style={{
+                  color: '#ffffff',
+                  fontSize: '40px',
+                }}
+              />
+            </a>
+          </Row>
+          <span className="footer__text" aria-label="copyright">
+            Copyright © 2022 Coderheroes Inc.
+          </span>
         </Col>
       </Row>
     </Footer>
   );
 }
-
-// CoderHeroes Social Media Links
-// https://www.facebook.com/coderheroes/
-// https://twitter.com/coderheroes/
-// https://www.instagram.com/coderheroes/
-// https://www.youtube.com/channel/UC7vHHesa12tznVpgvnwbnKg/
-// https://www.linkedin.com/company/coderheroes/
 
 export default MainFooter;

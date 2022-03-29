4394bc1c4067f2dc06314bd37ad7a5568dba1bfc
Raj-04 <1204patel.raj@gmail.com>
2022-03-29 12:00:10 -0400

    added the sidebar to the classes

--- src/components/pages/InstructorHome/AllClassesView.js
+++ src/components/pages/InstructorHome/AllClassesView.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import InstructorSidebar from '../InstructorHome/InstructorSidebar';
+import '../../../styles/InstructorStyles/index.less';
+import { Layout } from 'antd';
 
+const { Content } = Layout;
 const AllClasses = () => {
   return (
-    <div>
-      <h3>This is where all the classes will show up</h3>
-    </div>
+    <>
+      <Layout>
+        <InstructorSidebar />
+        <Content>
+          <h3>This is where all the classes will show up</h3>
+        </Content>
+      </Layout>
+    </>
   );
 };
 

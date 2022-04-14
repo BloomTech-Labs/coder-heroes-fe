e2b5767fd6a996767e72d42e92ca207c38c384bb
Raj-04 <1204patel.raj@gmail.com>
2022-04-14 12:35:58 -0400

    Rendering the feedback badge individual card

---
+++ src/components/pages/Classroom/FeedbackBadge.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, Avatar } from 'antd';
+
+const { Meta } = Card;
+
+const FeedbackBadges = ({ badge }) => {
+  return (
+    <div>
+      <Card className="badge__card" hoverable>
+        <Meta
+          className="badge__info"
+          avatar={
+            <Avatar
+              src={badge.image}
+              size={128}
+              className="badge__card__image"
+            />
+          }
+          title={badge.name}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default FeedbackBadges;

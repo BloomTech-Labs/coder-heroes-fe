f55665d1f168b28d63e8f74565d5467823ca0456
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-04-18 15:43:07 -0700

    Edited family home component to properly display sidebar, footer, and banner

--- src/components/pages/ParentFamily/ParentFamilyHome.js
+++ src/components/pages/ParentFamily/ParentFamilyHome.js
@@ -6,11 +6,19 @@ import ParentSidebar from '../ParentHome/ParentSidebar';
 // import { useParams } from 'react-router';
 // import CurrentCoursesDetails from './CurrentCoursesDetails';
 import '../../../styles/ParentStyles/index.less';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
 
 const ParentFamilyHome = () => {
+  const { Content, Sider } = Layout;
   const [studentInfo, setStudentInfo] = useState(null); //eslint-disable-line
   // const [modal, setModal] = useState(false);
   // const [modal2, setModal2] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
 
   useEffect(() => {
     const token = JSON.parse(localStorage.getItem(`okta-token-storage`));
@@ -30,83 +38,92 @@ const ParentFamilyHome = () => {
 
   return (
     <div className="family-page-container">
-      <ParentSidebar />
-      <div className="family-page-content">
-        <Banner />
-        <div className="profile-select-titles" style={{ color: '#6A0C49' }}>
-          <strong>PICK A PROFILE TO ACCESS</strong>
-        </div>
-        <div className="profile-card-container">
-          <div className="profile-card-containers">
-            <div className="profile-avatars"></div>
-            <div className="profile-select-titles">Parent Name</div>
-            <button className="profile-select-button">VIEW ACCOUNT</button>
+      <Layout style={{ width: '100%' }}>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={toggleCollapsed}
+          theme="light"
+        >
+          <div style={{ display: 'flex', justifyContent: 'center' }} />
+          <ParentSidebar />
+        </Sider>
+        <Content>
+          <Banner />
+          <div className="profile-select-titles" style={{ color: '#6A0C49' }}>
+            <strong>PICK A PROFILE TO ACCESS</strong>
           </div>
-          <div className="profile-card-containers">
-            <div className="profile-avatars"></div>
-            <div className="profile-select-titles">Student Name</div>
-            <button className="profile-select-button">VIEW ACCOUNT</button>
-          </div>
-        </div>
-        <div className="profile-select-logout-container">
-          <button className="family-page-logout-button">
-            LOG OUT OF SHARED ACCOUNT
-          </button>
-        </div>
-        {/* <div className="profile-card-container">
-          <div className="profile-details-headers">
-            <h1>Profile</h1>
-            <h1>Username</h1>
-            <h1>Password</h1>
-          </div>
-          <div className="profile-data">
-            <div>_____</div>
-            <div>_____</div>
-            <div>_____</div>
-          </div>
-        </div>
-        <div className="between-profile-students">
-          <h1 className="students-section-title">Students</h1>
-          <button
-            className="family-page-button"
-            onClick={() => setModal(!modal)}
-          >
-            Create New Student
-          </button>
-        </div>
-        <div className="student-card-container">
-          <h1 className="student-card-name">
-            <strong>
-              Student Username: {studentInfo ? studentInfo[0].username : ''}
-            </strong>
-          </h1>
-          <div className="student-details">
-            <div className="student-details-sides">
-              <h1>Email Address: _____</h1>
-              <h1>Current Courses: _____</h1>
-              <button
-                className="family-page-button"
-                onClick={() => setModal2(!modal2)}
-              >
-                Current Courses Details
-              </button>
+          <div className="profile-card-container">
+            <div className="profile-card-containers">
+              <div className="profile-avatars" />
+              <div className="profile-select-titles">Parent Name</div>
+              <button className="profile-select-button">VIEW ACCOUNT</button>
             </div>
-            <div className="student-details-sides">
-              <h1>Age: {studentInfo ? studentInfo[0].age : ''}</h1>
-              <h1> Past Courses: _____</h1>
-              <button className="family-page-button">
-                Add/Change Prerequisites
-              </button>
+            <div className="profile-card-containers">
+              <div className="profile-avatars" />
+              <div className="profile-select-titles">Student Name</div>
+              <button className="profile-select-button">VIEW ACCOUNT</button>
             </div>
           </div>
-        </div>
-        {modal && <CreateNewStudent setModal={setModal} />}
-        {modal2 && <CurrentCoursesDetails setModal={setModal2} />}
-          </div> */}
-        {/* </div> */}
-      </div>
+          <div className="profile-select-logout-container">
+            <button className="family-page-logout-button">
+              LOG OUT OF SHARED ACCOUNT
+            </button>
+          </div>
+        </Content>
+      </Layout>
     </div>
   );
 };
 
 export default ParentFamilyHome;
+// {/* <div className="profile-card-container">
+//           <div className="profile-details-headers">
+//             <h1>Profile</h1>
+//             <h1>Username</h1>
+//             <h1>Password</h1>
+//           </div>
+//           <div className="profile-data">
+//             <div>_____</div>
+//             <div>_____</div>
+//             <div>_____</div>
+//           </div>
+//         </div>
+//         <div className="between-profile-students">
+//           <h1 className="students-section-title">Students</h1>
+//           <button
+//             className="family-page-button"
+//             onClick={() => setModal(!modal)}
+//           >
+//             Create New Student
+//           </button>
+//         </div>
+//         <div className="student-card-container">
+//           <h1 className="student-card-name">
+//             <strong>
+//               Student Username: {studentInfo ? studentInfo[0].username : ''}
+//             </strong>
+//           </h1>
+//           <div className="student-details">
+//             <div className="student-details-sides">
+//               <h1>Email Address: _____</h1>
+//               <h1>Current Courses: _____</h1>
+//               <button
+//                 className="family-page-button"
+//                 onClick={() => setModal2(!modal2)}
+//               >
+//                 Current Courses Details
+//               </button>
+//             </div>
+//             <div className="student-details-sides">
+//               <h1>Age: {studentInfo ? studentInfo[0].age : ''}</h1>
+//               <h1> Past Courses: _____</h1>
+//               <button className="family-page-button">
+//                 Add/Change Prerequisites
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//         {modal && <CreateNewStudent setModal={setModal} />}
+//         {modal2 && <CurrentCoursesDetails setModal={setModal2} />}
+//           </div> */}

0462cc2dc591ee7c3bfa560f43d881e857ef24db
david-aihe <doa9200@gmail.com>
2022-04-19 19:42:13 -0400

    added ant design Steps to view registration progress

--- src/components/pages/Registration/HowManyStudents.js
+++ src/components/pages/Registration/HowManyStudents.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import '../../../styles/registration.less';
+import { Steps } from 'antd';
 
 export default function HowManyStudents() {
+  const { Step } = Steps;
   const [numStudents, setNumStudents] = useState(1);
 
   const handleChange = e => {
@@ -10,6 +12,15 @@ export default function HowManyStudents() {
 
   return (
     <div className="reg-content-container email-confirmation">
+      <div>
+        <Steps current={1} className="reg-progress">
+          <Step />
+          <Step title="" />
+          <Step title="" description="" />
+          <Step title="" description="" />
+        </Steps>
+      </div>
+
       <div className="content">
         <p className="color-one">
           How many student profiles would you like to create?

42343203cec29b877ddfd857a69d1283914de552
Jessica Kitchen <jess@Jessicas-Mini.lan>
2022-04-19 19:40:07 -0400

    added hamburger menu icon to collapsable sidebar on parent family home
    Co-authored-by: Dylan Baker <dylanmbaker@yahoo.com>
    Co-authored-by: Jessica Williams <jesswillcode@gmail.com>

--- src/components/pages/ParentHome/ParentSidebar.js
+++ src/components/pages/ParentHome/ParentSidebar.js
@@ -58,38 +58,24 @@ const ParentSideBar = props => {
             cursor: 'pointer',
           }}
         >
-          {collapsed ? (
-            <svg
-              viewBox="64 64 896 896"
-              focusable="false"
-              class=""
-              data-icon="right"
-              width="1em"
-              height="1em"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path d="M765.7 486.8L314.9 134.7A7.97 7.97 0 00302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 000-50.4z"></path>
-            </svg>
-          ) : (
-            <svg
-              viewBox="64 64 896 896"
-              focusable="false"
-              class=""
-              data-icon="left"
-              width="1em"
-              height="1em"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 000 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z"></path>
-            </svg>
-          )}
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 55 42"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0 0H55V7H0V0ZM0 17.5H55V24.5H0V17.5ZM0 35H55V42H0V35Z"
+              fill="#6A0C49"
+            />
+          </svg>
         </div>
         <Menu.Item
           key="coderheroes"
           icon={<ThunderboltFilled fontSize="150px" />}
           className="dashboard-logo"
+          style={{ pointerEvents: 'none' }}
         >
           Coderheroes
         </Menu.Item>

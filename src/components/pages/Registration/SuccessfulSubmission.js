4c10d344b34a23dfa9f706f61b12137a585b15d3
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2022-04-21 11:19:24 -0400

    finished styling for successful teacher registration

--- src/components/pages/Registration/SuccessfulSubmission.js
+++ src/components/pages/Registration/SuccessfulSubmission.js
@@ -8,10 +8,13 @@ export default function SuccessfulSubmission() {
         <p className="color-one">
           Your CoderHeroes Educator Application has been successfully submitted.
         </p>
-        <p className="color-one">
-          We look forward to contacting you in 2-3 business days. Thank you!
-        </p>
+        <div className="sub-content">
+          <p className="color-one">
+            We look forward to contacting you in 2-3 business days. Thank you!
+          </p>
+        </div>
         <div className="link-container">
+          {/* eslint-disable-next-line*/}
           <a href="#">LEARN MORE</a>
           {/* update href after finding correct path from Okta */}
         </div>

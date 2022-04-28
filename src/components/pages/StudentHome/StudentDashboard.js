43a79e501fc67aa4e1339f6f682e75a57a6a271b
Peter Conley <peterdavidconley@gmail.com>
2022-04-28 15:09:28 -0400

    adjusting padding, borders, and margins
    Co-authored-by: Peter Conley <peter.david.conley@gmail.com>
    Co-authored-by: Hannah Brog <bugsbrog@gmail.com>

--- src/components/pages/StudentHome/StudentDashboard.js
+++ src/components/pages/StudentHome/StudentDashboard.js
@@ -11,37 +11,51 @@ function StudentDashboard(props) {
     <div className="student-dashboard">
       <section className="studentdb-left-column">
         <div className="current-student-db">
-          <img></img>
           <center>
+            <img src="https://via.placeholder.com/100" alt="placeholder"></img>
             <h1>Student Name</h1>
           </center>
         </div>
         <div className="current-teacher-db">
-          <div>
-            <img></img>
-          </div>
-          <div>
-            <h2>Coder Camp</h2>
-            <h2>Teacher Name</h2>
-          </div>
+          <center>
+            <div>
+              <img src="https://via.placeholder.com/50" alt="placeholder"></img>
+            </div>
+            <div>
+              <h2>Coder Camp</h2>
+              <h2>Teacher Name</h2>
+            </div>
+          </center>
         </div>
       </section>
       <section className="studentdb-right-column">
         <section className="student-photo-tasks">
           <div className="Photo-Video-File">
-            <img></img>
-            <h5>Photo</h5>
-            <img></img>
-            <h5>Video</h5>
-            <img></img>
-            <h5>File</h5>
-            <img></img>
-            <h5>Write</h5>
-            <img></img>
-            <h5>Meet</h5>
+            <div>
+              <img src="https://via.placeholder.com/50" alt="placeholder"></img>
+              <h5>Photo</h5>
+            </div>
+            <div>
+              <img src="https://via.placeholder.com/50" alt="placeholder"></img>
+              <h5>Video</h5>
+            </div>
+            <div>
+              <img src="https://via.placeholder.com/50" alt="placeholder"></img>
+              <h5>File</h5>
+            </div>
+            <div>
+              <img src="https://via.placeholder.com/50" alt="placeholder"></img>
+              <h5>Write</h5>
+            </div>
+            <div>
+              <img src="https://via.placeholder.com/50" alt="placeholder"></img>
+              <h5>Meet</h5>
+            </div>
           </div>
           <div className="tasks-completed">
             <h3>Tasks Completed 3/10</h3>
+            <h3> || </h3>
+            <h3>New Stories 2</h3>
           </div>
         </section>
         <section className="student-stories">
@@ -49,7 +63,7 @@ function StudentDashboard(props) {
             <h2>Stories</h2>
           </div>
           <div className="student-story-card">
-            <img></img>
+            <img src="https://via.placeholder.com/100" alt="placeholder"></img>
             <h3>Teacher Name</h3>
             <h4>Coder Heroes</h4>
             <h4>9 Minutes Ago</h4>

c9d3837ec7a7cce9000b4adb7cb7438786a2fd82
KFamurewa <kayla.famurewa@gmail.com>
2022-04-28 12:13:05 -0400

    added registration progress component

--- src/components/pages/Registration/ParentWelcome.js
+++ src/components/pages/Registration/ParentWelcome.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import RegistrationProgress from './RegistrationProgress';
 import '../../../styles/registration.less';
 
 export default function ParentWelcome() {
   return (
     <div className="reg-content-container">
+      <RegistrationProgress step_num={0} />
       <div className="content">
         <p className="color-one">Welcome to CoderHeroes!</p>
         <p className="color-two">

7b9816dc86aafb82abf2550842ae19f1e8adb5f1
Peter Conley <peterdavidconley@gmail.com>
2022-04-28 14:30:35 -0400

    created stylesheet for StudentDashboard component, created base elements for page, outlined content for components
    Co-authored-by: Peter Conley <peter.david.conley@gmail.com>
    Co-authored-by: Hannah Brog <bugsbrog@gmail.com>

--- src/components/pages/StudentHome/StudentDashboard.js
+++ src/components/pages/StudentHome/StudentDashboard.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import '../../../styles/StudentStyles/index.less';
 
 const initialStudent = null;
 
@@ -7,8 +8,28 @@ function StudentDashboard(props) {
   const [student, setStudent] = useState(initialStudent);
 
   return (
-    <div>
-      <h1>Current Student</h1>
+    <div className="student-dashboard">
+      <section className="studentdb-left-column">
+        <h1>Current Student</h1>
+        <h2>Current Teacher</h2>
+      </section>
+      <section className="studentdb-right-column">
+        <section className="student-photo-tasks">
+          <h3>Photo-Video-File</h3>
+          <h3>Tasks Completed 3/10</h3>
+        </section>
+        <section className="student-stories">
+          <div>
+            <h2>Stories</h2>
+          </div>
+          <div>
+            <img></img>
+            <h3>Teacher Name</h3>
+            <h4>Coder Heroes</h4>
+            <h4>9 Minutes Ago</h4>
+          </div>
+        </section>
+      </section>
     </div>
   );
 }

ccdb26250afae1164dab1b3590247a1156af005d
Peter Conley <peterdavidconley@gmail.com>
2022-05-03 06:20:50 -0400

    created base HTML scaffolding for student story card
    Co-authored-by: Peter Conley <peter.david.conley@gmail.com>
    Co-authored-by: Hannah Brog <bugsbrog@gmail.com>

--- src/components/pages/StudentHome/StudentStoryCard.js
+++ src/components/pages/StudentHome/StudentStoryCard.js
@@ -5,10 +5,31 @@ import { connect } from 'react-redux';
 function StudentStoryCard(prop) {
   return (
     <div className="student-story-card">
-      <img src="https://via.placeholder.com/50" alt="placeholder"></img>
-      <h3>Teacher Name</h3>
-      <h4>Coder Heroes</h4>
-      <h4>9 Minutes Ago</h4>
+      <section className="top-left">
+        <img src="https://via.placeholder.com/50" alt="placeholder"></img>
+        <h3>Teacher Name</h3>
+        <h4>Coder Heroes</h4>
+      </section>
+      <section className="top-right">
+        <h4>9 Minutes Ago</h4>
+      </section>
+      <section className="teacher-message">
+        <h4>Teacher message</h4>
+        <p>URL</p>
+      </section>
+      <section className="story-photo">
+        <img></img>
+      </section>
+      <section className="student-comment">
+        <img></img>
+        <h4>Student Name</h4>
+        <p>Student Message</p>
+        <h4>4 M</h4>
+      </section>
+      <section className="student-send-comment">
+        <img></img>
+        {/* Create field and form here*/}
+      </section>
     </div>
   );
 }

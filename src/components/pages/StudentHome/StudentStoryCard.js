9f8d1b6eb0d6913c751daa4f9249f5af6b4bdeca
Peter Conley <peterdavidconley@gmail.com>
2022-05-03 14:22:43 -0400

    adjusted formatting of top layer of content for student story card
    Co-authored-by: Peter Conley <peter.david.conley@gmail.com>
    Co-authored-by: Hannah Brog <bugsbrog@gmail.com>

--- src/components/pages/StudentHome/StudentStoryCard.js
+++ src/components/pages/StudentHome/StudentStoryCard.js
@@ -5,15 +5,19 @@ import { Card } from 'antd';
 
 function StudentStoryCard(prop) {
   return (
-    <Card title="Teacher Name" className="student-story-card">
-      <section className="top-left">
-        <img src="https://via.placeholder.com/50" alt="placeholder"></img>
-        <h3>Teacher Name</h3>
-        <h4>Coder Heroes</h4>
-      </section>
-      <section className="top-right">
-        <h4>9 Minutes Ago</h4>
-      </section>
+    <Card className="student-story-card">
+      <div className="top-story-card">
+        <section className="top-left">
+          <img src="https://via.placeholder.com/50" alt="placeholder"></img>
+          <div>
+            <h2>Teacher Name</h2>
+            <h4>Coder Heroes</h4>
+          </div>
+        </section>
+        <section className="top-right">
+          <h4>9 Minutes Ago</h4>
+        </section>
+      </div>
       <section className="teacher-message">
         <h4>Teacher message</h4>
         <p>URL</p>

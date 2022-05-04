cb37bf59e719575996f8f071247c50ad6459da20
Jessica Kitchen <jess@Jessicas-Mini-112.lan>
2022-05-04 12:47:07 -0400

    added Carousel to resources page

--- src/components/pages/ParentHome/ParentResources.js
+++ src/components/pages/ParentHome/ParentResources.js
@@ -1,32 +1,19 @@
 import React from 'react';
+import Banner from '../../common/Banner';
+import ParentSidebar from '../ParentHome/ParentSidebar';
+import '../../../styles/ParentStyles/index.less';
+import { Layout, Carousel } from 'antd';
 import 'antd/dist/antd.css';
 import { Space, Card } from 'antd';
 import { MailOutlined } from '@ant-design/icons';
 
 const ParentResources = props => {
   return (
-    <div>
-      <h2 style={{ fontSize: '2.5rem', textAlign: 'center' }}>
-        {<MailOutlined />} News Feed
-      </h2>
-      <Space direction="vertical" style={{ width: '100%' }}>
-        <Card title="Mrs. Rodriguez" style={{ width: '100%' }}>
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
-        </Card>
-        <Card title="Mrs. Smith" style={{ width: '100%' }}>
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
-        </Card>
-        <Card title="Mrs. Lewis" style={{ width: '100%' }}>
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
-        </Card>
-      </Space>
-    </div>
+    <Layout>
+      <ParentSidebar active="dashboard" />
+      <Banner />
+      <Carousel />
+    </Layout>
   );
 };
 

bfd899d0a24113428cf55a20866f927f4693c195
Jessica Kitchen <jess@Jessicas-Mini-112.lan>
2022-05-04 13:56:25 -0400

    built out class cards and collapse panels for tasks

--- src/components/pages/ParentHome/ParentTasks.js
+++ src/components/pages/ParentHome/ParentTasks.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Banner from '../../common/Banner';
 import ParentSidebar from '../ParentHome/ParentSidebar';
-<<<<<<< HEAD
 import ParentNavbar from './ParentNavbar';
 import StudentDropdown from './StudentDropdown';
 import '../../../styles/ParentStyles/index.less';
@@ -53,17 +52,6 @@ const ParentTasks = () => {
           </Card>
         </Card>
       </Content>
-=======
-import '../../../styles/ParentStyles/index.less';
-import { Layout } from 'antd';
-import 'antd/dist/antd.css';
-
-const ParentTasks = () => {
-  return (
-    <Layout>
-      <ParentSidebar active="dashboard" />
-      <Banner />
->>>>>>> 200dbf8 (B)
     </Layout>
   );
 };

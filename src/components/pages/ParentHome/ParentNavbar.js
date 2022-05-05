aa2073b88de287c21ff508ffec3f9e4c8067a95a
Jessica Kitchen <jess@Jessicas-Mini-116.lan>
2022-05-04 23:31:30 -0400

    styled Parent Resources page

--- src/components/pages/ParentHome/ParentNavbar.js
+++ src/components/pages/ParentHome/ParentNavbar.js
@@ -5,9 +5,8 @@ import 'antd/dist/antd.css';
 import {
   ClockCircleOutlined,
   GiftOutlined,
-  UnorderedListOutlined,
   FundProjectionScreenOutlined,
-  UserOutlined,
+  CarryOutOutlined,
 } from '@ant-design/icons';
 //import { useOktaAuth } from '@okta/okta-react';
 
@@ -34,7 +33,6 @@ const ParentNavbar = props => {
       color="orange"
       inlineCollapsed={collapsed}
     >
-      {/* <div className='navItems'> */}
       <Menu.Item
         className="parentNavItem"
         key="progress"
@@ -52,7 +50,7 @@ const ParentNavbar = props => {
       <Menu.Item
         className="parentNavItem"
         key="tasks"
-        icon={<UnorderedListOutlined fontSize="150px" />}
+        icon={<CarryOutOutlined fontSize="150px" />}
       >
         <Link to="/parent/tasks">Tasks</Link>
       </Menu.Item>
@@ -63,7 +61,6 @@ const ParentNavbar = props => {
       >
         <Link to="/parent/resources">Resources</Link>
       </Menu.Item>
-      {/* </div> */}
     </Menu>
   );
 };

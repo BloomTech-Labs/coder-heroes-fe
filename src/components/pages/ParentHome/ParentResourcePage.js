3f583e9b74a9aec874eb851c56fdc2b10a904d9c
Jessica Kitchen <jess@Jessicas-Mini-120.lan>
2022-05-05 12:44:54 -0400

    added parent nav bar into mission control

--- src/components/pages/ParentHome/ParentResourcePage.js
+++ src/components/pages/ParentHome/ParentResourcePage.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import Banner from '../../common/Banner';
+<<<<<<< HEAD:src/components/pages/ParentHome/ParentResourcePage.js
 import ParentSidebar from '../ParentHome/ParentSidebar';
 import '../../../styles/ParentStyles/index.less';
 import { Layout } from 'antd';
 import 'antd/dist/antd.css';
 
 const ParentResourcesPage = () => {
+=======
+import ParentSidebar from './ParentSidebar';
+import ParentNavbar from './ParentNavbar';
+import StudentDropdown from './StudentDropdown';
+import '../../../styles/ParentStyles/dashboard.less';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
+
+const ParentDashboard = () => {
+  const { Content } = Layout;
+
+>>>>>>> 6d07931 (added parent nav bar into mission control):src/components/pages/ParentHome/parentDashboard.js
   return (
     <Layout>
       <ParentSidebar active="dashboard" />
-      <Banner />
+      <Content>
+        <Banner />
+        <div className="navDrop">
+          <ParentNavbar />
+          <StudentDropdown />
+        </div>
+      </Content>
     </Layout>
   );
 };

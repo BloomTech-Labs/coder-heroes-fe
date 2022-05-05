ab844474b7f9498e6bd900a11f3345647f4e13d3
Jessica Williams <jesswillcode@gmail.com>
2022-05-05 17:27:10 -0400

    Cleaning up warnings

--- src/components/pages/ParentHome/ParentHome.js
+++ src/components/pages/ParentHome/ParentHome.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ParentFamilyHome from '../ParentFamily/ParentFamilyHome';
 import { Layout } from 'antd';
 import 'antd/dist/antd.css';
-import ParentNavbar from './ParentNavbar';
 import Banner from '../../common/Banner';
-import ParentSidebar from './ParentSidebar';
 import './../../../styles/ParentStyles/index.less';
 
 function ParentHome() {
-  const { Content } = Layout;
 
   return (
     <Layout>

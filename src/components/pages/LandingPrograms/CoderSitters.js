b38eb27f06f62f6dea21ae63a21232e76f94d9a9
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2022-05-18 18:45:35 -0400

    Added a wrapper and flexbox to fix design in Codesitters page of Programs component

--- src/components/pages/LandingPrograms/CoderSitters.js
+++ src/components/pages/LandingPrograms/CoderSitters.js
@@ -8,50 +8,52 @@ export default function CoderSitters() {
   return (
     <Layout>
       <Content className="pl__na__sitter__container">
-        <div className="pl__na__top__sitter__container">
-          {/* CODERSITTERS TITLE */}
-          <Title className="pl__na__sitter__title">CODERSITTERS</Title>
-          {/* CODERSITTERS INTRO PARAGRAPH */}
-          <Text className="pl__na__intro__text">
-            Parents get their needed
-            <br />
-            distraction-free time. Kids get to
-            <br />
-            learn coding through play.
-          </Text>
-          {/* BABYSITTER + INSTRUCTOR */}
-          <div className="pl__na__top__mid__container">
-            <Title className="pl__na__sitter__title__container">
-              THINK BABY SITTER + CODING INSTRUCTOR.
+        <div className="pl__na__sitter__container_wrapper">
+          <div className="pl__na__top__sitter__container">
+            {/* CODERSITTERS TITLE */}
+            <Title className="pl__na__sitter__title" level={2}>
+              CODERSITTERS
             </Title>
-
-            {/* INFO PARAGRAPH */}
-            <Text className="pl__na__sitter__text__container">
-              Our lessons include playing age-appropriate
+            {/* CODERSITTERS INTRO PARAGRAPH */}
+            <Text className="pl__na__intro__text">
+              Parents get their needed
               <br />
-              coding games online, in tandem with a wide
+              distraction-free time. Kids get to
               <br />
-              range of interactive offline activities. These
-              <br />
-              might include: drawing, prototyping, crafting
-              <br />
-              or imaginative brainstorming
+              learn coding through play.
             </Text>
-          </div>
-          {/* AGE */}
-          <div className="pl__na__age__container">
-            <Text className="pl__na__age">Ages 6 and Up.</Text>
+            {/* BABYSITTER + INSTRUCTOR */}
+            <div className="pl__na__top__mid__container">
+              <Title className="pl__na__sitter__title__container" level={2}>
+                THINK BABY SITTER + CODING INSTRUCTOR.
+              </Title>
+
+              {/* INFO PARAGRAPH */}
+              <Text className="pl__na__sitter__text__container">
+                Our lessons include playing age-appropriate
+                <br />
+                coding games online, in tandem with a wide
+                <br />
+                range of interactive offline activities. These
+                <br />
+                might include: drawing, prototyping, crafting
+                <br />
+                or imaginative brainstorming
+              </Text>
+            </div>
+            {/* AGE */}
+            <div className="pl__na__age__container">
+              <Text className="pl__na__age">Ages 6 and Up.</Text>
+            </div>
+
+            {/* BUTTON */}
+            <div className="pl__na__button__container">
+              <button className="pl__na__sitter__button" type="submit">
+                Learn More
+              </button>
+            </div>
           </div>
 
-          {/* BUTTON */}
-          <div className="pl__na__button__container">
-            <button className="pl__na__sitter__button" type="submit">
-              Learn More
-            </button>
-          </div>
-        </div>
-
-        <div>
           <div className="pl__na__image__container">
             <img
               className="pl__na__top__right__collage"

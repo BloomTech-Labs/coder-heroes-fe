1718e6f17b306c2ca4d156110b3a918a9b224867
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-05-19 15:16:08 -0400

    removed parent navbar

--- src/components/pages/ParentHome/ParentDashboard.js
+++ src/components/pages/ParentHome/ParentDashboard.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Banner from '../../common/Banner';
 import ParentSidebar from './ParentSidebar';
 import IndividualNewsParent from '../ParentNewsFeed/IndividualNewsParent';
-import MessageList from './Messages/MessageList';
-import ParentNavbar from './ParentNavbar';
 import '../../../styles/ParentStyles/dashboard.less';
 import { Layout, Col, Row, Card } from 'antd';
 import 'antd/dist/antd.css';
@@ -15,14 +13,11 @@ const ParentDashboard = () => {
       <ParentSidebar active="dashboard" />
       <Content>
         <Banner />
-        <div className="navDrop">
-          <ParentNavbar />
-        </div>
         <Content className="dashboard-container">
           <Row className="row">
             <Col className="parent-dashboard-column" span={10}>
               <Row className="row">
-                <Card className="dashboard-card ">
+                <Card className="dashboard-card">
                   <h2>NewsFeed</h2>
                   <Row className="row">
                     <IndividualNewsParent />

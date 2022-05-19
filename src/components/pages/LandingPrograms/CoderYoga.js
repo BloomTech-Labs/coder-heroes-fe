d0410b22ea585e230897fd3923dd86ec9e06ebcc
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2022-05-19 15:11:28 -0400

    Aligned all text on landing pages to center and resized certain components to keep with continuity.

--- src/components/pages/LandingPrograms/CoderYoga.js
+++ src/components/pages/LandingPrograms/CoderYoga.js
@@ -27,7 +27,7 @@ export default function CoderYoga() {
           <div className="pl__na__right">
             <div className="pl__na__right__container">
               <Title className="pl__na__coder__yoga__title" level={2}>
-                CODERYOGA
+                CODER-YOGA
               </Title>
               <Text className="pl__na__right__info__container" level={3}>
                 Kids learn coding basics through

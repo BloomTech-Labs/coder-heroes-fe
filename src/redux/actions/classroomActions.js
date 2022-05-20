bfb9dbcbee50d2020dd2ca788cc40421d34c6956
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-05-19 18:50:18 -0600

    Created badge actions refactored feedBackBadge.
    --Edited feedback page to be called from button on student card
    --Badge page exists to give feedback
    --Attached buttons to feedback badges that add or remove
    --Created and Refactored action creators for adding and removing badges
    from backend
    --Conditional rendering logic on feedbackBadgePage for badges that are
    earned or not earned
    
    **TO DO**
    --Add visual cues to badges to indicate completion status
    --Style Style Style
    --Fix badge removal bug hook more aspects of badge adding and removal
    into state
    --Figure out rerender in regards to badge page
    
    Co-Author: Will Phillips <42WPhillips42@gmail.com>

--- src/redux/actions/classroomActions.js
+++ src/redux/actions/classroomActions.js
@@ -6,6 +6,9 @@ export const GET_BADGES_BY_ID = 'GET_BADGES_BY_ID';
 export const SET_CURRENT_STUDENT_ID = 'SET_CURRENT_STUDENT_ID';
 export const ERROR = 'ERROR';
 export const FETCHING = 'FETCHING';
+export const GET_BADGES = 'GET_BADGES';
+export const ADD_BADGE_TO_STUDENT = 'ADD_BADGE_TO_STUDENT';
+export const REMOVE_BADGE_FROM_STUDENT = 'REMOVE_BADGE_FROM_STUDENT';
 
 export const setCourseId = course_id => {
   return { type: SET_COURSE_ID, payload: course_id };
@@ -35,6 +38,24 @@ export const getStudents = (idToken, course_id) => async dispatch => {
     });
   }
 };
+export const getBadges = idToken => async dispatch => {
+  dispatch({
+    type: FETCHING,
+    payload: true,
+  });
+  try {
+    const res = await axiosWithAuth(idToken).get(`/classroom/badges/`);
+    dispatch({
+      type: GET_BADGES,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ERROR,
+      payload: true,
+    });
+  }
+};
 
 export const getBadgesById = (idToken, student_id) => async dispatch => {
   dispatch({
@@ -48,6 +69,59 @@ export const getBadgesById = (idToken, student_id) => async dispatch => {
     dispatch({
       type: GET_BADGES_BY_ID,
       payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ERROR,
+      payload: true,
+    });
+  }
+};
+
+export const addBadgeToStudent = (
+  idToken,
+  badge_id,
+  student_id,
+  badge
+) => async dispatch => {
+  dispatch({
+    type: FETCHING,
+    payload: true,
+  });
+  try {
+    axiosWithAuth(idToken).post(`/classroom/assign`, {
+      badge_id: badge_id,
+      child_id: student_id,
+    });
+    dispatch({
+      type: ADD_BADGE_TO_STUDENT,
+      payload: badge,
+    });
+  } catch (error) {
+    dispatch({
+      type: ERROR,
+      payload: true,
+    });
+  }
+};
+
+export const removeBadgeFromStudent = (
+  idToken,
+  badge_id,
+  student_id,
+  badge
+) => async dispatch => {
+  dispatch({
+    type: FETCHING,
+    payload: true,
+  });
+  try {
+    axiosWithAuth(idToken).delete(
+      `/classroom/remove/${badge_id}/${student_id}`
+    );
+    dispatch({
+      type: REMOVE_BADGE_FROM_STUDENT,
+      payload: badge,
     });
   } catch (error) {
     dispatch({

74af52c6d9171f66c42948b559573064b2a9099f
lailaarkadan <laila.arkadan@gmail.com>
2022-05-26 12:56:14 -0600

    added button to AdminSidebar,  added space for styles,and created AdminApplications route.

---
+++ src/components/pages/AdminHome/AdminApplications.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdminSidebar from './AdminSidebar';
+import '../../../styles/index.less';
+
+export default function AdminApplications() {
+  return (
+    <div className="admin-applications-container">
+      <AdminSidebar />
+    </div>
+  );
+}

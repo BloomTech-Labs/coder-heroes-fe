b1aea54f5bdac4c826e774eec89fe0368e8428c3
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2022-05-31 09:27:28 -0400

    Updated ParentDashboard.js to allow push to this branch to work.

--- src/components/pages/ParentHome/parentDashboard.js
+++ src/components/pages/ParentHome/parentDashboard.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import Banner from '../../common/Banner';
 import ParentSidebar from './ParentSidebar';
-import ParentNavbar from './ParentNavbar';
-import StudentDropdown from './StudentDropdown';
 import '../../../styles/ParentStyles/dashboard.less';
 import { Layout } from 'antd';
 import 'antd/dist/antd.css';
 
 const ParentDashboard = () => {
-  const { Content } = Layout;
-
   return (
     <Layout>
       <ParentSidebar active="dashboard" />
-      <Content>
-        <Banner />
-        <div className="navDrop">
-          <ParentNavbar />
-          <StudentDropdown />
-        </div>
-      </Content>
+      <Banner />
     </Layout>
   );
 };

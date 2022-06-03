23535e1a597e64fef059b430b84936a575809c61
WRWPhillips <42WPhillips42@gmail.com>
2022-06-03 16:27:57 -0500

    Fixed final fixable error!!!
    -- There is another warning in console from React concerning the prop
    type within routers. Upon further research this seems to have to do with
    the syntax of our entire application, and the libraries we have. It is
    not fixable without completely changing every component, and it's only a
    warning, so I'm not sweating it! Give it a try, it's great. No warnings!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import { createRoot } from 'react-dom/client';
 import { Layout } from 'antd';
 import {
   BrowserRouter as Router,
@@ -73,7 +74,10 @@ import InstructorFlow_Step2 from './components/pages/Registration/InstructorFlow
 const store = createStore(rootReducers, applyMiddleware(thunk));
 window.store = store; // Remove before full deployment. In here for development purposes.
 
-ReactDOM.render(
+const app = document.getElementById('root');
+const root = createRoot(app);
+
+root.render(
   <Provider store={store}>
     <Router>
       <React.StrictMode>
@@ -90,8 +94,7 @@ ReactDOM.render(
         </Layout>
       </React.StrictMode>
     </Router>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
 
 function App() {

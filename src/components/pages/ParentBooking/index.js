3f8360f8d7b892910ee0904a25d840e21b6580f1
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-06-08 12:54:17 -0400

    removed from ParentBooking/index.js

--- src/components/pages/ParentBooking/index.js
+++ src/components/pages/ParentBooking/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ParentSidebar from '../ParentHome/ParentSidebar';
 import Banner from '../../common/Banner';
-import ParentNavbar from '../ParentHome/ParentNavbar';
 import { Layout } from 'antd';
 import ParentBookingContainer from './ParentBookingContainer';
 import '../../../styles/ParentStyles/booking.less';
@@ -14,7 +13,6 @@ function ParentBooking() {
       <ParentSidebar active="courses" />
       <Content>
         <Banner />
-        <ParentNavbar />
 
         <ParentBookingContainer className="booking-container" />
       </Content>

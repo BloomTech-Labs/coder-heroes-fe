c998c688cfedd05cd4f8a60f15a9e4c1a9de6296
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-06-21 12:36:25 -0400

    BL-147 Workaround for local login issue implemented. Added User action types and reducer functions for getting conversations by profileId

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,4 +1,9 @@
-import { GET_CURRENT_USER, SET_ERROR } from '../actions/userActions';
+import {
+  GET_CURRENT_USER,
+  SET_ERROR,
+  GET_CONVERSATIONS_ACTION,
+  GET_CONVERSATIONS_SUCCESS,
+} from '../actions/userActions';
 
 const initialState = {
   currentUser: {},
@@ -11,6 +16,16 @@ const reducer = (state = initialState, action) => {
       return { ...state, currentUser: action.payload };
     case SET_ERROR:
       return { ...state, errorMessage: action.payload };
+    case GET_CONVERSATIONS_ACTION:
+      return {
+        ...state,
+        conversations: { isFetching: true, conversations: [] },
+      };
+    case GET_CONVERSATIONS_SUCCESS:
+      return {
+        ...state,
+        conversations: { isFetching: false, conversations: action.payload },
+      };
     default:
       return state;
   }

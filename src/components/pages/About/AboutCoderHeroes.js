b67462e3444029e83340da6ee6bd7422fac7e34f
Kevin Lee <kevin-lee@lambdastudents.comgit config --global user.email kevkenshi@gmail.com>
2022-07-12 12:53:37 -0700

    BL-462

--- src/components/pages/About/AboutCoderHeroes.js
+++ src/components/pages/About/AboutCoderHeroes.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import { Layout, Typography } from 'antd';
+import '../../../styles/AboutStyles/about.less';
+const { Content } = Layout;
+
+const { Title } = Typography;
 
 export default function AboutCoderHeroes() {
   return (
-    <div>
-      <h1>About CoderHeroes</h1>
-      <p>
-        This is the page where the user can read about CoderHeroes website
-        information before registering for enrollment.
-      </p>
-    </div>
+    <Layout>
+      <Content className="aboutContainer">
+        <div className="aboutPolicy">
+          <div className="aboutWrapper">
+            <Title className="about">ABOUT CODERHEROES</Title>
+            <p className="aboutText">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+              fugiat iusto fuga praesentium optio, eaque rerum! Provident
+              similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+              eius earum ut molestias architecto voluptate aliquam nihil,
+              eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+              tenetur error, harum nesciunt ipsum debitis quas aliquid.
+              Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa
+              laudantium molestias eos sapiente officiis modi at sunt excepturi
+              expedita sint? Sed quibusdam recusandae alias error harum maxime
+              adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+              Officiis iure rerum voluptates a cumque velit quibusdam sed amet
+              tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat,
+              temporibus enim commodi iusto libero magni deleniti quod quam
+              consequuntur! Commodi minima excepturi repudiandae velit hic
+              maxime doloremque. Quaerat provident commodi consectetur veniam
+              similique ad earum omnis ipsum saepe, voluptas, hic voluptates
+              pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
+              excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
+              Voluptatem quaerat non architecto ab laudantium modi minima sunt
+              esse temporibus sint culpa, recusandae aliquam numquam totam
+              ratione voluptas quod exercitationem fuga. Possimus quis earum
+              veniam quasi aliquam eligendi, placeat qui corporis!
+            </p>
+          </div>
+        </div>
+      </Content>
+    </Layout>
   );
 }

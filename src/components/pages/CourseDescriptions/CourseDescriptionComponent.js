50d9c78610432f4cc9596e2cfc2ca22ab15e06e2
AnthonyLayne <kepheus.ds@gmail.com>
2022-07-19 22:37:31 -0400

    BL-518 started styling for cards

--- src/components/pages/CourseDescriptions/CourseDescriptionComponent.js
+++ src/components/pages/CourseDescriptions/CourseDescriptionComponent.js
@@ -1,35 +1,48 @@
 import React from 'react';
 import { Card, Button } from 'antd';
+import '../../../styles/BookingStyles/CourseDescriptionStyles.less';
+import avatar from '../../../styles/BookingStyles/assets/Avatar07.png';
+
+//const { Content } = Layout;
 
 function CourseDescriptionComponent() {
   return (
-    <div>
-      <div classname="top-image">
-        <div classname="img-container">
-          <img classname="img" src="" />
-        </div>
-        <div classname="title-container">
+    <div className="course-page">
+      <div className="page-title">
+        <img className="image" src={avatar} alt="Student" />
+
+        <div className="title-container">
           <h1>Course Descriptions</h1>
         </div>
       </div>
-      <div classname="card-container">
+      <div className="card-container">
         <Card
           className="description-card"
           title="CoderYoga"
-          extra={<a href="#">More</a>}
           style={{
-            width: 300,
             border: true,
             borderStyle: 'solid',
             borderColor: 'red',
           }}
           headStyle={{
-            backgroundColor: 'black',
+            backgroundColor: '#9FB222',
+            height: '129px',
+            fontFamily: 'Montserrat',
+            fontStyle: 'bold',
+            fontWeight: 700,
+            fontSize: '40px',
+            lineHeight: '22px',
+            textAlign: 'center',
+            display: 'flex',
+            justifyContent: 'center',
           }}
         >
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris
+          </p>
+
           <Button className="bttn" type="primary">
             Register Now
           </Button>
@@ -40,6 +53,9 @@ function CourseDescriptionComponent() {
           extra={<a href="#">More</a>}
           style={{
             width: 300,
+          }}
+          headStyle={{
+            backgroundColor: '#FEAD2A',
           }}
         >
           <p>Card content</p>
@@ -55,6 +71,9 @@ function CourseDescriptionComponent() {
           extra={<a href="#">More</a>}
           style={{
             width: 300,
+          }}
+          headStyle={{
+            backgroundColor: '#B9E3D0',
           }}
         >
           <p>Card content</p>

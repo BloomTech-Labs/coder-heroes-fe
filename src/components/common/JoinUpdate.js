848dbb390094b25b4d344db6aff7fca1219b350d
Landon Phillips <landonphillips@landon.com>
2022-07-20 12:37:31 -0500

    working on loggin state

--- src/components/common/JoinUpdate.js
+++ src/components/common/JoinUpdate.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useOktaAuth } from '@okta/okta-react';
+
+// console.log(localStorage.getItem("token"));
 const initialForm = {
   'First Name': '',
   'Last Name': '',
@@ -8,7 +11,18 @@ const initialForm = {
   Notes: '',
 };
 const Form = props => {
+  // const { authService, authState } = useOktaAuth();
   const [value, setValues] = useState(initialForm);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // console.log(authState.isAuthenticated);
+    if (!localStorage.getItem('token')) {
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+  }, []);
   const onChange = event => {
     const name = event.target.name;
     setValues({ ...value, [name]: event.target.value });
@@ -33,7 +47,7 @@ const Form = props => {
         },
         {
           headers: {
-            Authorization: 'Bearer keyvivxw89Lg0dwjm',
+            Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
           },
         }
       )
@@ -54,97 +68,111 @@ const Form = props => {
   };
   return (
     <div>
-      <h2 style={{}}>Join Our Updates Now!</h2>
+      {!isLoggedIn ? (
+        <>
+          <h2>Join Our Updates Now!</h2>{' '}
+          <form onSubmit={onSubmit}>
+            <fieldset>
+              <label
+                style={{
+                  display: 'inline-block',
+                  float: 'left',
+                  width: '80%',
+                  'text-align': 'right',
+                }}
+              >
+                I am..
+                <select name="Role" onChange={onChange}>
+                  <option name="parent" value={props.values}>
+                    Parent
+                  </option>
+                  <option name="instructor" value={props.values}>
+                    Instructor
+                  </option>
+                </select>
+              </label>
 
-      <form onSubmit={onSubmit}>
-        <label
-          style={{
-            display: 'inline-block',
-            float: 'left',
-            width: '80%',
-            'text-align': 'right',
-          }}
-        >
-          I am..
-          <select name="Role" onChange={onChange}>
-            <option name="parent" value={props.values}>
-              Parent
-            </option>
-            <option name="instructor" value={props.values}>
-              Instructor
-            </option>
-          </select>
-        </label>
+              <label
+                style={{
+                  display: 'inline-block',
+                  float: 'left',
+                  width: '80%',
+                  'text-align': 'right',
+                }}
+              >
+                First Name:
+                <input
+                  style={{ border: '1px solid black' }}
+                  type="text"
+                  placeholder="Enter First Name.."
+                  name="First Name"
+                  value={props.values}
+                  onChange={onChange}
+                />
+              </label>
 
-        <label
-          style={{
-            display: 'inline-block',
-            float: 'left',
-            width: '80%',
-            'text-align': 'right',
-          }}
-        >
-          First Name:
-          <input
-            style={{ border: '1px solid black' }}
-            type="text"
-            placeholder="Enter First Name.."
-            name="First Name"
-            value={props.values}
-            onChange={onChange}
-          />
-        </label>
+              <label
+                style={{
+                  display: 'inline-block',
+                  float: 'left',
+                  width: '80%',
+                  'text-align': 'right',
+                }}
+              >
+                Last Name:
+                <input
+                  style={{ border: '1px solid black' }}
+                  type="text"
+                  placeholder="Enter Last Name.."
+                  name="Last Name"
+                  value={props.values}
+                  onChange={onChange}
+                />
+              </label>
 
-        <label
-          style={{
-            display: 'inline-block',
-            float: 'left',
-            width: '80%',
-            'text-align': 'right',
-          }}
-        >
-          Last Name:
-          <input
-            style={{ border: '1px solid black' }}
-            type="text"
-            placeholder="Enter Last Name.."
-            name="Last Name"
-            value={props.values}
-            onChange={onChange}
-          />
-        </label>
+              <label
+                style={{ float: 'left', width: '80%', 'text-align': 'right' }}
+              >
+                Email Address:
+                <input
+                  style={{ border: '1px solid black' }}
+                  type="email"
+                  placeholder="Enter Email.."
+                  name="Email Address"
+                  value={props.values}
+                  onChange={onChange}
+                />
+              </label>
 
-        <label style={{ float: 'left', width: '80%', 'text-align': 'right' }}>
-          Email Address:
-          <input
-            style={{ border: '1px solid black' }}
-            type="email"
-            placeholder="Enter Email.."
-            name="Email Address"
-            value={props.values}
-            onChange={onChange}
-          />
-        </label>
+              <label
+                style={{ float: 'left', width: '80%', 'text-align': 'right' }}
+              >
+                Notes:
+                <input
+                  style={{ border: '1px solid black' }}
+                  type="text"
+                  placeholder="Add a Message.."
+                  name="Notes"
+                  value={props.values}
+                  onChange={onChange}
+                />
+              </label>
 
-        <label style={{ float: 'left', width: '80%', 'text-align': 'right' }}>
-          Notes:
-          <input
-            style={{ border: '1px solid black' }}
-            type="text"
-            placeholder="Add a Message.."
-            name="Notes"
-            value={props.values}
-            onChange={onChange}
-          />
-        </label>
-
-        <div style={{ float: 'left', width: '76%', 'text-align': 'right' }}>
-          <input type="submit" value="Join Now!" />
-        </div>
-      </form>
+              <div
+                style={{ float: 'left', width: '76%', 'text-align': 'right' }}
+              >
+                <input type="submit" value="Join Now!" />
+              </div>
+            </fieldset>
+          </form>
+        </>
+      ) : (
+        <h2>HELP MEEEE!</h2>
+      )}
     </div>
   );
 };
 export default Form;
+
 //johns api call https://api.airtable.com/v0/app4rHdRcowslAwKp/Table%201 keyvivxw89Lg0dwjm
 // our api call https://api.airtable.com/v0/appSgakw7GmVkbEVl/user%20info keyFVhRW3Vr2Gtxha

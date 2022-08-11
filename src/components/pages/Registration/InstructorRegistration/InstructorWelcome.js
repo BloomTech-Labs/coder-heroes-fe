44da7090dedd1d36a054a8ac12eb3f69d91c29f3
Landon Phillips <landonphillips@landon.com>
2022-08-11 14:58:46 -0500

    some refactoring

--- src/components/pages/Registration/InstructorRegistration/InstructorWelcome.js
+++ src/components/pages/Registration/InstructorRegistration/InstructorWelcome.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../../../../styles/registration.less';
 import RegistrationProgress from '../RegistrationProgress';
-import RegistrationProgress from '../RegistrationProgress';
 
 export default function InstructorWelcome() {
   return (

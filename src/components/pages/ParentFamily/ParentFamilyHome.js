5535d032526975d85805fb486e3dc936b9cca996
David Shipuk <dshipuk@yahoo.com>
2022-08-16 16:52:10 -0700

    Removed console.log from ParentFamilyHome

--- src/components/pages/ParentFamily/ParentFamilyHome.js
+++ src/components/pages/ParentFamily/ParentFamilyHome.js
@@ -37,8 +37,6 @@ const ParentFamilyHome = props => {
   const dispatch = useDispatch();
   const { user, children } = props;
 
-  console.log(props);
-
   useEffect(() => {
     dispatch(getChildren(idToken, user.profile_id));
   }, [dispatch, idToken, user.profile_id]);

5bc48ff5b1e182e208e584b85567b0a3426d4e23
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-08-25 12:25:24 -0600

    CoursesCard set up with dummy data, renders on /admin-courses

--- src/components/pages/AdminHome/AdminCourseCard.js
+++ src/components/pages/AdminHome/AdminCourseCard.js
@@ -2,11 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { Card } from 'antd';
 
 export default function CourseCard(props) {
-  const { courses } = props;
+  const { course } = props;
 
   return (
     <div className="admin-course-card-container">
-      <Card title={courses.course_name} />
+      <Card title={course.course_name}>
+        Program Name: {course.program_name} <br />
+        Instructor: {course.instructor}
+        <br />
+        Date: <br />
+      </Card>
     </div>
   );
 }
--- src/components/pages/AdminHome/AdminCourses.js
+++ src/components/pages/AdminHome/AdminCourses.js
@@ -6,12 +6,23 @@ import axiosWithAuth from '../../../utils/axiosWithAuth';
 
 import CourseCard from './AdminCourseCard';
 
-const initialCoursesState = {};
+const initialCoursesState = [
+  {
+    course_name: 'Intro to JS',
+    instructor: 'John Doe',
+    program_name: 'CoderYoga',
+  },
+  {
+    course_name: 'Intro to Python',
+    instructor: 'Jane Doe',
+    program_name: 'CoderSitters',
+  },
+];
 
 export default function AdminCourses() {
   const { authState } = useOktaAuth();
   const { idToken } = authState;
-  const [courses, setCourses] = useState([initialCoursesState]);
+  const [courses, setCourses] = useState(initialCoursesState);
 
   useEffect(() => {
     axiosWithAuth(idToken)
@@ -26,7 +37,7 @@ export default function AdminCourses() {
     <div className="admin-courses-container">
       <AdminSidebar />
       {courses.map(item => (
-        <CourseCard key={Date.now()} courses={courses} />
+        <CourseCard key={Date.now()} course={item} />
       ))}
     </div>
   );

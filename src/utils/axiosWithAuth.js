9d2309fd382312b9a9bf6fadb56fc6778b301cee
paulstgermain <paulg.stgermain@gmail.com>
2022-08-25 14:17:22 -0400

    fixes axiosWithAuth to properly add axios idToken to auth header

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ const axiosWithAuth = idToken => {
   return axios.create({
     baseURL: process.env.REACT_APP_API_URI,
     headers: {
-      Authorization: 'Bearer ' + idToken.idToken,
+      Authorization: 'Bearer ' + idToken,
     },
   });
 };

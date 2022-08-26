2fed9e93831bf94082de4f60ba777515a5bb862d
Lisa DeSpain <lisamdespain@gmail.com>
2022-08-26 18:44:27 -0500

    course page and cards are styled as well as the add course button

--- src/components/pages/AdminHome/AdminCourseCard.js
+++ src/components/pages/AdminHome/AdminCourseCard.js
@@ -1,19 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+// removed , { useHistory } from above line
 import { Card } from 'antd';
 
 export default function CourseCard(props) {
   const { course } = props;
+  // const history = useHistory();
+
+  // const handleEditCourse = () =>{
+  //   history.push('/admin-add-course');
+  // };
+
+  // const viewCourseDetails = (id) =>{
+  //   history.push(`/admin-course-details/:id`);
+  // };
 
   return (
     <div className="admin-course-card-container">
       <Card title={course.course_name}>
-        Program Name: {course.program_name} <br />
-        Instructor: {course.instructor}
-        <br />
-        Date: {course.date}
-        <br />
-        <button>EDIT COURSE</button>
-        <button>VIEW COURSE</button>
+        <h3>
+          Program Name: <span className="black">{course.program_name}</span>
+        </h3>
+        <h3>
+          Instructor: <span className="black">{course.instructor}</span>
+        </h3>
+
+        <h3>
+          Date: <span className="black">{course.date}</span>
+        </h3>
+
+        <div className="courses-button-container">
+          <button className="courses-button">EDIT COURSE</button>
+          {/* onClick={handleEditCourse} put back in edit course button? */}
+          <button className="courses-button">VIEW COURSE</button>
+          {/* onClick={viewCourseDetails} put back in view course button? */}
+        </div>
       </Card>
     </div>
   );
--- src/components/pages/AdminHome/AdminCourses.js
+++ src/components/pages/AdminHome/AdminCourses.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+// removed useHistory for now
 import AdminSidebar from './AdminSidebar';
 import '../../../styles/index.less';
 import { useOktaAuth } from '@okta/okta-react';
 import axiosWithAuth from '../../../utils/axiosWithAuth';
-
+import { Layout } from 'antd';
 import CourseCard from './AdminCourseCard';
+import AdminAddCourses from '../AdminAddCourse';
 
 const initialCoursesState = [
   {
@@ -17,6 +19,34 @@ const initialCoursesState = [
   {
     course_id: 2,
     course_name: 'Intro to Python',
+    instructor: 'Jane Doe',
+    program_name: 'CoderSitters',
+    date: '12/01/2023',
+  },
+  {
+    course_id: 3,
+    course_name: 'Intro to CSS',
+    instructor: 'Mark Smith',
+    program_name: 'CoderSitters',
+    date: '12/01/2023',
+  },
+  {
+    course_id: 4,
+    course_name: 'Intro to HTML',
+    instructor: 'Jane Doe',
+    program_name: 'CoderCamp',
+    date: '12/01/2023',
+  },
+  {
+    course_id: 5,
+    course_name: 'Intro to HTML',
+    instructor: 'Another Person',
+    program_name: 'CoderYoga',
+    date: '12/01/2023',
+  },
+  {
+    course_id: 6,
+    course_name: 'Intro to React',
     instructor: 'Jane Doe',
     program_name: 'CoderSitters',
     date: '12/01/2023',
@@ -37,12 +67,33 @@ export default function AdminCourses() {
       .catch(err => console.error(err));
   }, []);
 
+  // const history = useHistory();
+
+  // const handleClick = () =>{
+  //   history.push('/admin-add-course');
+  // };
+
+  const { Content } = Layout;
+
   return (
-    <div className="admin-courses-container">
+    <Layout>
       <AdminSidebar />
-      {courses.map(item => (
-        <CourseCard key={item.course_id} course={item} />
-      ))}
-    </div>
+      <Content>
+        <div class="table-container">
+          <div class="header-container">
+            <h1>Courses</h1>
+          </div>
+          <div class="courses-right">
+            <button className="add-course">ADD COURSE</button>
+          </div>
+          {/*  onClick={<AdminAddCourses />} put back in button click */}
+          <div className="admin-courses-container">
+            {courses.map(item => (
+              <CourseCard key={item.course_id} course={item} />
+            ))}
+          </div>
+        </div>
+      </Content>
+    </Layout>
   );
 }

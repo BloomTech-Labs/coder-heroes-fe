3bfdee5f2a3d65c6977879e9b459f6e1e83f9402
unknown <macrowell@yahoo.com>
2022-09-01 11:32:19 -0400

    updates styles on instructor cards to match styles of other cards

--- src/components/pages/AdminHome/AdminInstructorCard.js
+++ src/components/pages/AdminHome/AdminInstructorCard.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Card, Button } from 'antd';
+import { Card } from 'antd';
 
 function AdminInstructorCard(props) {
   const { name, bio, status } = props;
 
   return (
     <Card title={name} className="Instructor-Card">
-      <p className="card-content">{`Bio: ${bio}`}</p>
-      <p className="card-content">{`Approval Status: ${status}`}</p>
-      <Button className="edit-button">Edit</Button>
+      <h3>
+        Bio: <span className="black">{`${bio}`}</span>
+      </h3>
+      <h3>
+        Approval Status: <span className="black">{`${status}`}</span>
+      </h3>
+      <div className="instructor-button-container">
+        <button className="instructor-button">Edit</button>
+        <button className="instructor-button">View</button>
+      </div>
     </Card>
   );
 }

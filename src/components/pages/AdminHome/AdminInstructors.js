cc0807dc6fbd810fb7747fb53e30762269c7f169
unknown <macrowell@yahoo.com>
2022-09-01 17:14:30 -0400

    fixes toggle and cleans up some unnecessary code

--- src/components/pages/AdminHome/AdminInstructors.js
+++ src/components/pages/AdminHome/AdminInstructors.js
@@ -1,47 +1,69 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { getInstructors } from '../../../redux/actions/instructorActions';
 import { useOktaAuth } from '@okta/okta-react';
 import AdminSidebar from './AdminSidebar';
 import { Layout } from 'antd';
-import AdminPendingInstructors from './AdminPendingInstructors';
-import AdminActiveInstructors from './AdminActiveInstructors';
+import AdminInstructorCard from './AdminInstructorCard';
+
+const instructors = [];
+
+for (let i = 0; i < 10; i++) {
+  instructors.push({
+    name: `test${i}`,
+    bio: `test${i}`,
+    status: `${i % 2 == 0 ? 'true' : 'false'}`,
+  });
+}
 
 function AdminInstructors(props) {
-  const { instructors } = props;
+  // const { instructors } = props;
+  const filtered = instructors.filter(
+    instructor => instructor.status === 'false'
+  );
+  const [pending, setPending] = useState(true);
+  const [displayed, setDisplayed] = useState(filtered);
   const dispatch = useDispatch();
   const idToken = useOktaAuth().oktaAuth.getIdToken();
 
   const ToggleInstructors = () => {
-    dispatch(!instructors);
+    if (pending) {
+      setDisplayed(instructors);
+      setPending(false);
+    }
+    if (!pending) {
+      setDisplayed(filtered);
+      setPending(true);
+    }
   };
 
   useEffect(() => {
     dispatch(getInstructors(idToken));
   }, [dispatch, idToken]);
 
-  const history = useHistory();
-
   const { Content } = Layout;
 
   return (
     <Layout>
       <AdminSidebar />
-
       <Content>
         <div className="header-container">
           <div className="button-container">
             <button className="toggle-button" onClick={ToggleInstructors}>
-              {' '}
-              {!instructors ? 'Pending Instructors' : 'Active Instructors'}{' '}
+              {!pending ? 'Pending Instructors' : 'Active Instructors'}
             </button>
-            {instructors ? (
-              <AdminPendingInstructors instructors={instructors} />
-            ) : (
-              <AdminActiveInstructors instructors={instructors} />
-            )}
+            <div className="card-container">
+              {displayed.map(instructor => (
+                <AdminInstructorCard
+                  name={instructor.name}
+                  bio={instructor.bio}
+                  status={
+                    instructor.status === 'false' ? 'Pending' : 'Approved'
+                  }
+                />
+              ))}
+            </div>
           </div>
         </div>
       </Content>

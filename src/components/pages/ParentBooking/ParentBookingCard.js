d320b5ea51f11025f96215d68fc7402ff60c1bc1
kevin-liu <liu.kevin1997@gmail.com>
2022-09-02 17:06:10 -0400

    Created the form to search for the courses

--- src/components/pages/ParentBooking/ParentBookingCard.js
+++ src/components/pages/ParentBooking/ParentBookingCard.js
@@ -1,81 +1,126 @@
 // WE ARE CURRENTLY TRYING OUT THE SingleBookingComponent.js PLEASE REFER TO THAT COMPONENT FOR BOOKING FOR NOW
-
 import React from 'react';
 import { connect } from 'react-redux';
-import { Card, Button } from 'antd';
+import { Typography, Input, Select, Layout, Form } from 'antd';
 import { dateConverter } from '../../common/dateHelpers';
 import { timeConverter } from '../../common/timeHelpers';
 import axiosWithAuth from '../../../utils/axiosWithAuth';
 import { useOktaAuth } from '@okta/okta-react';
 import { addToCart } from '../../../redux/actions/parentActions';
+import { dummyData } from '../../../dummyData';
 
 const ParentBookingCard = props => {
   const {
-    child_name,
-    subject,
-    description,
+    course_name,
+    course_description,
     start_date,
     end_date,
     start_time,
     end_time,
+    number_of_sessions,
     location,
-    instructor_name,
+    days_of_week,
     size,
-    course_id,
+    min_age,
+    max_age,
   } = props.booking;
 
+  const { Content } = Layout;
   const { addToCart } = props;
+  const { Item } = Form;
+  const { Option } = Select;
+  const { Title } = Typography;
 
   const { authState } = useOktaAuth();
   const { idToken } = authState;
+
   const handleClick = e => {
     axiosWithAuth(idToken)
-      .post(
-        '/children/1/enrollments', // TODO: Hook this request up to pass the ID of the parent/child involved once we have this data in state.
-        { child_id: 1, class_id: course_id, completed: true }
-      )
+      .post()
       .then(res => console.log(res)) // TODO: Let's perform some action with this result.
       .catch(err => console.log(`message: ${err.message}`));
   };
 
-  const data = [
-    { title: 'student name', text: child_name },
-    { title: 'course', text: subject },
-    { title: 'description', text: description },
-    { title: 'first day of class', text: dateConverter(start_date) },
-    { title: 'last day of class', text: dateConverter(end_date) },
-    {
-      title: 'time',
-      text: `${timeConverter(start_time)} - ${timeConverter(end_time)}`,
-    },
-    { title: 'location', text: location },
-    { title: 'instructor', text: instructor_name },
-    { title: 'class size', text: size },
-  ];
+  // const data = [
+  //   { title: 'student name', text: child_name },
+  //   { title: 'course', text: subject },
+  //   { title: 'description', text: description },
+  //   { title: 'first day of class', text: dateConverter(start_date) },
+  //   { title: 'last day of class', text: dateConverter(end_date) },
+  //   {
+  //     title: 'time',
+  //     text: `${timeConverter(start_time)} - ${timeConverter(end_time)}`,
+  //   },
+  //   { title: 'location', text: location },
+  //   { title: 'instructor', text: instructor_name },
+  //   { title: 'class size', text: size },
+  // ];
 
   const handleAddCourse = booking => {
     addToCart(booking);
   };
 
   return (
-    <Card title={subject} style={{ width: 280 }} hoverable="true">
-      {data.map((itm, idx) => {
-        return (
-          <div key={idx}>
-            {itm.title}: {itm.text}
+    <Layout className="il__top">
+      <Content className="il__top__topContent">
+        <div className="il__top__topContent__container">
+          <div className="il__top__topContent__main">
+            <Title
+              className="il__heading il__top__topContent__heading"
+              level={2}
+            >
+              SEARCH FOR AVAILABLE COURSES
+            </Title>
+            <Form className="il__top__form" size={'large'} layout="inline">
+              <Input.Group compact>
+                <Item name={'speciality'}>
+                  <Select
+                    style={{ border: '1px solid #9DA7AB' }}
+                    placeholder="Choose a Program"
+                    allowClear
+                  >
+                    <Option value="JavaScriptB">JavaScriptB</Option>
+                    <Option value="'CS101'">CS101</Option>
+                  </Select>
+                </Item>
+                <Item name={'search'} noStyle>
+                  <Input
+                    type="date"
+                    className="il__top__input"
+                    style={{ border: '1px solid #9DA7AB' }}
+                    placeholder="Start Date"
+                    allowClear
+                    name="search"
+                    // <Select>
+                    //   <Option> <CalendarCard/></Option>
+                    // </Select>
+                  />
+                </Item>
+                <Item name={'availability'}>
+                  <Select
+                    style={{ border: '1px solid #9DA7AB' }}
+                    placeholder="Preferred Time of Day"
+                    allowClear
+                  >
+                    <Option value="morning">Morning</Option>
+                    <Option value="afternoon">Afternoon</Option>
+                    <Option value="night">Night</Option>
+                    <Option value="weekends">Weekends</Option>
+                  </Select>
+                </Item>
+                <button
+                  className="il__top__formBtn"
+                  type="submit"
+                  // onClick={handleSubmit}
+                >
+                  SEARCH
+                </button>
+              </Input.Group>
+            </Form>
           </div>
-        );
-      })}
-      <Button
-        type="primary"
-        style={{ background: '#006C72', color: 'white' }}
-        block
-        onClick={() => handleAddCourse(data)}
-      >
-        {' '}
-        ADD{'  '}
-      </Button>
-    </Card>
+        </div>
+      </Content>
+    </Layout>
   );
 };
 

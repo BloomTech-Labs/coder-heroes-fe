82685b75d0d775f6569c9cf3e0a63c53687538a9
Laila Arkadan <91079490+lailaarkadan@users.noreply.github.com>
2022-09-08 22:22:45 -0600

    implemented  design from figma

---
+++ src/components/pages/ParentBooking/BookingCalendar.js
@@ -0,0 +1,16 @@
+import { Calendar } from 'antd';
+import React from 'react';
+
+const BookingCalendar = () => {
+  const onPanelChange = (value, mode) => {
+    console.log(value.format('YYYY-MM-DD'), mode);
+  };
+
+  return (
+    <div className="site-calendar-demo-card">
+      <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+    </div>
+  );
+};
+
+export default BookingCalendar;
--- src/components/pages/ParentBooking/ParentBookingCard.js
+++ src/components/pages/ParentBooking/ParentBookingCard.js
@@ -1,13 +1,14 @@
 // WE ARE CURRENTLY TRYING OUT THE SingleBookingComponent.js PLEASE REFER TO THAT COMPONENT FOR BOOKING FOR NOW
 import React from 'react';
 import { connect } from 'react-redux';
-import { Typography, Input, Select, Layout, Form } from 'antd';
+import { Typography, Input, Select, Layout, Form, Calendar } from 'antd';
 import { dateConverter } from '../../common/dateHelpers';
 import { timeConverter } from '../../common/timeHelpers';
 import axiosWithAuth from '../../../utils/axiosWithAuth';
 import { useOktaAuth } from '@okta/okta-react';
 import { addToCart } from '../../../redux/actions/parentActions';
 import { dummyData } from '../../../dummyData';
+import BookingCalendar from './BookingCalendar';
 
 const ParentBookingCard = props => {
   const {
@@ -28,7 +29,7 @@ const ParentBookingCard = props => {
   const { Content } = Layout;
   const { addToCart } = props;
   const { Item } = Form;
-  const { Option } = Select;
+  const { button } = Select;
   const { Title } = Typography;
 
   const { authState } = useOktaAuth();
@@ -68,53 +69,269 @@ const ParentBookingCard = props => {
             <Title
               className="il__heading il__top__topContent__heading"
               level={2}
+              style={{ color: '#3a072c', textAlign: 'center' }}
             >
-              SEARCH FOR AVAILABLE COURSES
+              BOOK WITH US.
+              <br />
+              LEARN MORE THAN JUST CODE!
             </Title>
             <Form className="il__top__form" size={'large'} layout="inline">
-              <Input.Group compact>
-                <Item name={'speciality'}>
-                  <Select
-                    style={{ border: '1px solid #9DA7AB' }}
-                    placeholder="Choose a Program"
-                    allowClear
+              <Input.Group
+                compact
+                // style={{ display: 'flex', flexDirection: 'column' }}
+              >
+                <div>
+                  <div
+                    style={{
+                      color: '#3a072c',
+                      fontSize: '21px',
+                      fontWeight: 'bold',
+                    }}
                   >
-                    <Option value="JavaScriptB">JavaScriptB</Option>
-                    <Option value="'CS101'">CS101</Option>
-                  </Select>
-                </Item>
-                <Item name={'search'} noStyle>
-                  <Input
-                    type="date"
-                    className="il__top__input"
-                    style={{ border: '1px solid #9DA7AB' }}
-                    placeholder="Start Date"
-                    allowClear
-                    name="search"
-                    // <Select>
-                    //   <Option> <CalendarCard/></Option>
-                    // </Select>
-                  />
-                </Item>
-                <Item name={'availability'}>
-                  <Select
-                    style={{ border: '1px solid #9DA7AB' }}
-                    placeholder="Preferred Time of Day"
-                    allowClear
+                    Select Program
+                  </div>
+                  <Item name={'speciality'}>
+                    <Select
+                      style={{
+                        border: '1px solid #9DA7AB',
+                        margin: '5px 0 20px 0',
+                      }}
+                      placeholder="Choose a Program"
+                      allowClear
+                    >
+                      <button value="JavaScriptB">JavaScriptB</button>
+                      <button value="'CS101'">CS101</button>
+                    </Select>
+                  </Item>
+
+                  <div
+                    style={{
+                      color: '#3a072c',
+                      fontSize: '21px',
+                      fontWeight: 'bold',
+                      margin: ' 20px 0 15px 0',
+                    }}
                   >
-                    <Option value="morning">Morning</Option>
-                    <Option value="afternoon">Afternoon</Option>
-                    <Option value="night">Night</Option>
-                    <Option value="weekends">Weekends</Option>
-                  </Select>
-                </Item>
-                <button
-                  className="il__top__formBtn"
-                  type="submit"
-                  // onClick={handleSubmit}
+                    Select Time
+                  </div>
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                  }}
                 >
-                  SEARCH
-                </button>
+                  <Item style={{ flex: '3', paddingBottom: '30px' }}>
+                    <BookingCalendar
+                      style={{
+                        width: '300px',
+                        border: '1px solid #f0f0f0',
+                        borderRadius: '2px',
+                      }}
+                    />
+                  </Item>
+
+                  <Item
+                    name={'availability'}
+                    style={{
+                      display: 'flex',
+                      // flexWrap: 'wrap',
+                      minWidth: '450px',
+                      flexDirection: 'column',
+                      flex: '1',
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: 'flex',
+                        // flexWrap: 'wrap',
+                        justifyContent: 'space-around',
+                      }}
+                    >
+                      <button
+                        style={{
+                          borderRadius: '10px',
+                          padding: '10px 2px',
+                          margin: '6px',
+                          flex: '1',
+                          backgroundColor: '#3BC9B5',
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#680049',
+                        }}
+                      >
+                        9:00 AM
+                      </button>
+                      <button
+                        style={{
+                          borderRadius: '10px',
+                          padding: '10px 2px',
+                          margin: '6px',
+                          flex: '1',
+                          backgroundColor: '#3BC9B5',
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#680049',
+                        }}
+                      >
+                        10:00 AM
+                      </button>
+                      <button
+                        style={{
+                          borderRadius: '10px',
+                          padding: '10px 2px',
+                          margin: '6px',
+                          flex: '1',
+                          backgroundColor: '#3BC9B5',
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#680049',
+                        }}
+                      >
+                        11:00 AM
+                      </button>
+                    </div>
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                      }}
+                    >
+                      <button
+                        style={{
+                          borderRadius: '10px',
+                          padding: '10px 2px',
+                          margin: '6px',
+                          flex: '1',
+                          backgroundColor: '#3BC9B5',
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#680049',
+                        }}
+                      >
+                        12:00 PM
+                      </button>
+                      <button
+                        style={{
+                          borderRadius: '10px',
+                          padding: '10px 2px',
+                          margin: '6px',
+                          flex: '1',
+                          backgroundColor: '#3BC9B5',
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#680049',
+                        }}
+                      >
+                        1:00 PM
+                      </button>
+                      <button
+                        style={{
+                          borderRadius: '10px',
+                          padding: '10px 2px',
+                          margin: '6px',
+                          flex: '1',
+                          backgroundColor: '#3BC9B5',
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#680049',
+                        }}
+                      >
+                        2:00 PM
+                      </button>
+                    </div>
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                      }}
+                    >
+                      <button
+                        style={{
+                          borderRadius: '10px',
+                          padding: '10px 2px',
+                          margin: '6px',
+                          flex: '1',
+                          backgroundColor: '#3BC9B5',
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#680049',
+                        }}
+                      >
+                        3:00 PM
+                      </button>
+                      <button
+                        style={{
+                          borderRadius: '10px',
+                          padding: '10px 2px',
+                          margin: '6px',
+                          flex: '1',
+                          backgroundColor: '#3BC9B5',
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#680049',
+                        }}
+                      >
+                        4:00 PM
+                      </button>
+                      <button
+                        style={{
+                          borderRadius: '10px',
+                          padding: '10px 2px',
+                          margin: '6px',
+                          flex: '1',
+                          backgroundColor: '#3BC9B5',
+                          fontSize: '16px',
+                          fontWeight: 'bold',
+                          color: '#680049',
+                        }}
+                      >
+                        5:00 PM
+                      </button>
+                    </div>
+                    <div
+                      style={{
+                        backgroundColor: 'white',
+                        borderRadius: '5px',
+                        margin: '6px',
+                      }}
+                    >
+                      <p
+                        style={{
+                          color: '#6E4964',
+                          fontSize: '16px',
+                          margin: '6px',
+                          padding: '7px 0',
+                          textAlign: 'center',
+                        }}
+                      >
+                        All times are in Central Standard Time (US & Canada)
+                      </p>
+                    </div>
+                    <div
+                      style={{
+                        display: 'flex',
+                        flex: '1',
+                      }}
+                    >
+                      <button
+                        className="il__top__formBtn"
+                        type="submit"
+                        style={{
+                          backgroundColor: '#680049',
+                          color: 'white',
+                          flex: '1',
+                          margin: '6px',
+                          fontSize: '16px',
+                          padding: '7px 0',
+                        }}
+                        // onClick={handleSubmit}
+                      >
+                        CONTINUE BOOKING
+                      </button>
+                    </div>
+                  </Item>
+                </div>
               </Input.Group>
             </Form>
           </div>

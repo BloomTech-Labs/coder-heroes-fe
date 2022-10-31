22dea45668ab2801ac979624a04a49100ce105d2
unknown <epeng1994@gmail.com>
2022-10-31 11:44:46 -0400

    Promise resolved childAction.js

--- src/redux/actions/childAction.js
+++ src/redux/actions/childAction.js
@@ -9,34 +9,37 @@ export const GET_COURSES_SUCCESS = 'GET_COURSES_SUCCESS';
 
 export const getChildren = idToken => dispatch => {
   dispatch({ type: GET_CHILDREN_ACTION });
-  Promise.resolve({
-    child: {
-      child_id: 0,
-      profile_id: 0,
-      username: '',
-      age: 0,
-      parent_id: 0,
-    },
-  });
-  // axiosWithAuth(idToken)
+  // Promise.resolve({
+  //   child: {
+  //     child_id: 0,
+  //     profile_id: 0,
+  //     username: '',
+  //     age: 0,
+  //     parent_id: 0,
+  //   },
+  // })
+  // .then(value=>{
+  //   axiosWithAuth(idToken)
   //   .get(`${process.env.REACT_APP_API_URI}/children`)
   //   .then(res => {
-  //     dispatch({ type: GET_CHILDREN_SUCCESS, payload: res.data });
+  //     dispatch({ type: GET_CHILDREN_SUCCESS, payload: res.data || value});
   //   })
   //   .catch(err => {
   //     dispatch({ type: ERROR_ACTION, payload: err });
   //   });
+  // })
 };
 
 export const getCourses = (idToken, child_id) => dispatch => {
   dispatch({ type: GET_COURSES_ACTION });
-  Promise.resolve({ courses: {} });
-  // axiosWithAuth(idToken)
+  // Promise.resolve({ courses: {} }).then(value=>{
+  //   axiosWithAuth(idToken)
   //   .get(`/children/${child_id}/enrollments`)
   //   .then(res => {
-  //     dispatch({ type: GET_COURSES_SUCCESS, payload: res.data.enrollments });
+  //     dispatch({ type: GET_COURSES_SUCCESS, payload: res.data.enrollments || value});
   //   })
   //   .catch(err => {
   //     dispatch({ type: ERROR_ACTION, payload: err });
   //   });
+  // });
 };
